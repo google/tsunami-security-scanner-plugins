bde3947c9c8421148d0cd6ebd58efd47a6226f0b
Maciej Trzos <mtrzos@google.com>
2020-11-02 16:27:35 +0100

    Fix unintended changes.

--- example_vuln_detector/src/test/java/com/google/tsunami/plugins/example/ExampleVulnDetectorTest.java
+++ example_vuln_detector/src/test/java/com/google/tsunami/plugins/example/ExampleVulnDetectorTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.tsunami.plugins.detectors.cve.cve_2017_5638;
+package com.google.tsunami.plugins.example;
 
 import static com.google.common.truth.extensions.proto.ProtoTruth.assertThat;
 
