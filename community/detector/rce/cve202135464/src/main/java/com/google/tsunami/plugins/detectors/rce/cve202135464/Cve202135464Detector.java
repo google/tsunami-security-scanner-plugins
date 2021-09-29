5681f1de069ec364e351dfd5339039b7db928cc3
0xtavi <octavianfeodot2@gmail.com>
2021-09-29 09:43:35 +0300

    Adding CVE-2021-35464 detector plugin

--- community/detector/rce/cve202135464/src/main/java/com/google/tsunami/plugins/detectors/rce/cve202135464/Cve202135464Detector.java
+++ community/detector/rce/cve202135464/src/main/java/com/google/tsunami/plugins/detectors/rce/cve202135464/Cve202135464Detector.java
@@ -138,8 +138,8 @@ public final class Cve202135464Detector implements VulnDetector {
                         + "parameter on multiple pages. The exploitation does not require "
                         + "authentication, and remote code execution can be triggered by "
                         + "sending a single crafted /ccversion/* request to the server. "
-						+ "The vulnerability exists due to the usage of Sun ONE Application "
-						+ "Framework (JATO) found in versions of Java 8 or earlier. The issue " 
+						+ "The vulnerability exists due to the usage of Sun ONE Application "	
+						+ "Framework (JATO) found in versions of Java 8 or earlier. The issue "
                         + "was fixed in commit a267913b97002228c2df45f849151e9c373bc47f from "
                         + "OpenIdentityPlatform/OpenAM:master."))
         .build();
