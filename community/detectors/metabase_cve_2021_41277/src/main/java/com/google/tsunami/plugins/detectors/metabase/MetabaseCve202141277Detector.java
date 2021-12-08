c98fda4312ad316e25c5f1edce1ce5a883cd4f2f
C4o <syttcasd@gmail.com>
2021-12-08 08:46:23 +0800

    Update
    Update after reviews

--- community/detectors/metabase_cve_2021_41277/src/main/java/com/google/tsunami/plugins/detectors/metabase/MetabaseCve202141277Detector.java
+++ community/detectors/metabase_cve_2021_41277/src/main/java/com/google/tsunami/plugins/detectors/metabase/MetabaseCve202141277Detector.java
@@ -108,9 +108,9 @@ public final class MetabaseCve202141277Detector implements VulnDetector {
         .setVulnerability(
             Vulnerability.newBuilder()
                 .setMainId(VulnerabilityId.newBuilder().setPublisher("TSUNAMI_COMMUNITY")
-                    .setValue("Metabase CVE-2021-41277 Local File Inclusion Vulnerability"))
+                    .setValue("CVE_2021_41277"))
                 .setSeverity(Severity.CRITICAL)
-                .setTitle("CVE_2021_41277")
+                .setTitle("Metabase CVE-2021-41277 Local File Inclusion Vulnerability")
                 .setDescription("Metabase is an open source data analytics platform. In affected "
                     + "versions a security issue has been discovered with the custom GeoJSON map "
                     + "(`admin->settings->maps->custom maps->add a map`) support and potential "
