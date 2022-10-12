fdab65eddfffbca787f88f175f1b2cd20db2d4ae
Moritz Wilhelm <mzwm@google.com>
2022-10-12 09:42:50 -0700

    Add PathParameterInjection implementing the InjectionPoint interface.
    PiperOrigin-RevId: 480646976
    Change-Id: I1eeed039ccf212bdd43473c5a2a76909a066ac9f

---
+++ google/detectors/directorytraversal/generic_path_traversal_detector/src/main/java/com/google/tsunami/plugins/detectors/directorytraversal/genericpathtraversaldetector/PathParameterInjection.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.tsunami.plugins.detectors.directorytraversal.genericpathtraversaldetector;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.tsunami.common.net.UrlUtils;
+import com.google.tsunami.common.net.http.HttpRequest;
+import java.net.URI;
+
+/**
+ * An {@code InjectionPoint} that plants payloads at the root, current path, and frequently
+ * encountered path prefixes of the url.
+ */
+final class PathParameterInjection implements InjectionPoint {
+  private static final String[] commonPaths = {
+    // go/keep-sorted start
+    "admin",
+    "album",
+    "app",
+    "assets",
+    "bin",
+    "console",
+    "css",
+    "demo",
+    "doc",
+    "eqx",
+    "files",
+    "fs",
+    "html",
+    "img-sys",
+    "jquery_ui",
+    "js",
+    "media",
+    "public",
+    "scripts",
+    "static",
+    "tmp",
+    "upload",
+    "xls",
+    // go/keep-sorted end
+  };
+
+  private HttpRequest buildModifiedRequest(HttpRequest request, String url) {
+    return request.toBuilder().setUrl(url).build();
+  }
+
+  private String extractRoot(URI url) {
+    return url.getScheme() + "://" + url.getRawAuthority();
+  }
+
+  @Override
+  public ImmutableSet<HttpRequest> injectPayload(HttpRequest request, String payload) {
+    ImmutableSet.Builder<HttpRequest> builder = ImmutableSet.builder();
+    for (String target : this.generateFuzzTargets(URI.create(request.url()), payload)) {
+      builder.add(this.buildModifiedRequest(request, target));
+    }
+    return builder.build();
+  }
+
+  private ImmutableSet<String> generateFuzzTargets(URI url, String payload) {
+    return ImmutableSet.<String>builder()
+        .addAll(this.generateTargetAtRoot(url, payload))
+        .addAll(this.generateTargetAtCurrentPath(url, payload))
+        .addAll(this.generateTargetAtCommonPaths(url, payload))
+        .build();
+  }
+
+  private ImmutableSet<String> generateTargetAtRoot(URI url, String payload) {
+    return ImmutableSet.of(this.extractRoot(url) + "/" + payload);
+  }
+
+  private ImmutableSet<String> generateTargetAtCurrentPath(URI url, String payload) {
+    String path = UrlUtils.removeTrailingSlashes(url.getRawPath());
+    String prefix = "";
+    int endOfParent = path.lastIndexOf("/");
+    if (endOfParent != -1) {
+      prefix = path.substring(0, endOfParent);
+    }
+    return ImmutableSet.of(this.extractRoot(url) + prefix + "/" + payload);
+  }
+
+  private ImmutableSet<String> generateTargetAtCommonPaths(URI url, String payload) {
+    ImmutableSet.Builder<String> builder = ImmutableSet.builder();
+    for (String commonPath : commonPaths) {
+      builder.add(this.extractRoot(url) + "/" + commonPath + "/" + payload);
+    }
+    return builder.build();
+  }
+}
---
+++ google/detectors/directorytraversal/generic_path_traversal_detector/src/test/java/com/google/tsunami/plugins/detectors/directorytraversal/genericpathtraversaldetector/PathParameterInjectionTest.java
@@ -0,0 +1,194 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.tsunami.plugins.detectors.directorytraversal.genericpathtraversaldetector;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.tsunami.common.net.http.HttpRequest;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Unit tests for {@link PathParameterInjection}. */
+@RunWith(JUnit4.class)
+public final class PathParameterInjectionTest {
+  private static final PathParameterInjection INJECTION_POINT = new PathParameterInjection();
+
+  @Test
+  public void injectPayload_onRelativePathTraversalPayload_generatesExploitsForRoot() {
+    HttpRequest exploitAtRoot =
+        HttpRequest.get("https://google.com/../../../../etc/passwd").withEmptyHeaders().build();
+
+    assertThat(
+            INJECTION_POINT.injectPayload(
+                HttpRequest.get("https://google.com/path/to/file").withEmptyHeaders().build(),
+                "../../../../etc/passwd"))
+        .contains(exploitAtRoot);
+  }
+
+  @Test
+  public void injectPayload_onRelativePathTraversalPayload_generatesExploitsForCurrentPath() {
+    HttpRequest exploitAtCurrentPath =
+        HttpRequest.get("https://google.com/path/to/../../../../etc/passwd")
+            .withEmptyHeaders()
+            .build();
+
+    assertThat(
+            INJECTION_POINT.injectPayload(
+                HttpRequest.get("https://google.com/path/to/file").withEmptyHeaders().build(),
+                "../../../../etc/passwd"))
+        .contains(exploitAtCurrentPath);
+  }
+
+  @Test
+  public void injectPayload_onRelativePathTraversalPayload_generatesExploitsForCommonPaths() {
+    ImmutableSet<String> targets =
+        ImmutableSet.of(
+            // go/keep-sorted start
+            "https://google.com/admin/../../../../etc/passwd",
+            "https://google.com/album/../../../../etc/passwd",
+            "https://google.com/app/../../../../etc/passwd",
+            "https://google.com/assets/../../../../etc/passwd",
+            "https://google.com/bin/../../../../etc/passwd",
+            "https://google.com/console/../../../../etc/passwd",
+            "https://google.com/css/../../../../etc/passwd",
+            "https://google.com/demo/../../../../etc/passwd",
+            "https://google.com/doc/../../../../etc/passwd",
+            "https://google.com/eqx/../../../../etc/passwd",
+            "https://google.com/files/../../../../etc/passwd",
+            "https://google.com/fs/../../../../etc/passwd",
+            "https://google.com/html/../../../../etc/passwd",
+            "https://google.com/img-sys/../../../../etc/passwd",
+            "https://google.com/jquery_ui/../../../../etc/passwd",
+            "https://google.com/js/../../../../etc/passwd",
+            "https://google.com/media/../../../../etc/passwd",
+            "https://google.com/public/../../../../etc/passwd",
+            "https://google.com/scripts/../../../../etc/passwd",
+            "https://google.com/static/../../../../etc/passwd",
+            "https://google.com/tmp/../../../../etc/passwd",
+            "https://google.com/upload/../../../../etc/passwd",
+            "https://google.com/xls/../../../../etc/passwd"
+            // go/keep-sorted end
+            );
+    Set<HttpRequest> requests = new HashSet<>();
+    for (String target : targets) {
+      requests.add(HttpRequest.get(target).withEmptyHeaders().build());
+    }
+
+    assertThat(
+            INJECTION_POINT.injectPayload(
+                HttpRequest.get("https://google.com/path/to/file").withEmptyHeaders().build(),
+                "../../../../etc/passwd"))
+        .containsAtLeastElementsIn(requests);
+  }
+
+  @Test
+  public void injectPayload_onAbsolutePathTraversalPayload_generatesExploitsForRoot() {
+    HttpRequest exploitAtRoot =
+        HttpRequest.get("https://google.com//etc/passwd").withEmptyHeaders().build();
+
+    assertThat(
+            INJECTION_POINT.injectPayload(
+                HttpRequest.get("https://google.com/path/to/file").withEmptyHeaders().build(),
+                "/etc/passwd"))
+        .contains(exploitAtRoot);
+  }
+
+  @Test
+  public void injectPayload_onAbsolutePathTraversalPayload_generatesExploitsForCurrentPath() {
+    HttpRequest exploitAtCurrentPath =
+        HttpRequest.get("https://google.com/path/to//etc/passwd").withEmptyHeaders().build();
+
+    assertThat(
+            INJECTION_POINT.injectPayload(
+                HttpRequest.get("https://google.com/path/to/file").withEmptyHeaders().build(),
+                "/etc/passwd"))
+        .contains(exploitAtCurrentPath);
+  }
+
+  @Test
+  public void injectPayload_onAbsolutePathTraversalPayload_generatesExploitsForCommonPaths() {
+    ImmutableSet<String> targets =
+        ImmutableSet.of(
+            // go/keep-sorted start
+            "https://google.com/admin//etc/passwd",
+            "https://google.com/album//etc/passwd",
+            "https://google.com/app//etc/passwd",
+            "https://google.com/assets//etc/passwd",
+            "https://google.com/bin//etc/passwd",
+            "https://google.com/console//etc/passwd",
+            "https://google.com/css//etc/passwd",
+            "https://google.com/demo//etc/passwd",
+            "https://google.com/doc//etc/passwd",
+            "https://google.com/eqx//etc/passwd",
+            "https://google.com/files//etc/passwd",
+            "https://google.com/fs//etc/passwd",
+            "https://google.com/html//etc/passwd",
+            "https://google.com/img-sys//etc/passwd",
+            "https://google.com/jquery_ui//etc/passwd",
+            "https://google.com/js//etc/passwd",
+            "https://google.com/media//etc/passwd",
+            "https://google.com/public//etc/passwd",
+            "https://google.com/scripts//etc/passwd",
+            "https://google.com/static//etc/passwd",
+            "https://google.com/tmp//etc/passwd",
+            "https://google.com/upload//etc/passwd",
+            "https://google.com/xls//etc/passwd"
+            // go/keep-sorted end
+            );
+    Set<HttpRequest> requests = new HashSet<>();
+    for (String target : targets) {
+      requests.add(HttpRequest.get(target).withEmptyHeaders().build());
+    }
+
+    assertThat(
+            INJECTION_POINT.injectPayload(
+                HttpRequest.get("https://google.com/path/to/file").withEmptyHeaders().build(),
+                "/etc/passwd"))
+        .containsAtLeastElementsIn(requests);
+  }
+
+  @Test
+  public void injectPayload_whenInjectionAtRoot_doesNotGenerateAdditionalExploitsAtCurrentPath() {
+    assertThat(
+            INJECTION_POINT
+                .injectPayload(
+                    HttpRequest.get("https://google.com").withEmptyHeaders().build(),
+                    "../../../../etc/passwd")
+                .size())
+        .isLessThan(
+            INJECTION_POINT
+                .injectPayload(
+                    HttpRequest.get("https://google.com/path/to/file").withEmptyHeaders().build(),
+                    "../../../../etc/passwd")
+                .size());
+  }
+
+  @Test
+  public void injectPayload_whenInjectionAtRoot_ignoresTrailingSlash() {
+    assertThat(
+            INJECTION_POINT.injectPayload(
+                HttpRequest.get("https://google.com").withEmptyHeaders().build(),
+                "../../../../etc/passwd"))
+        .containsExactlyElementsIn(
+            INJECTION_POINT.injectPayload(
+                HttpRequest.get("https://google.com/").withEmptyHeaders().build(),
+                "../../../../etc/passwd"));
+  }
+}
