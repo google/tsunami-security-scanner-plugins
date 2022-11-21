cd5b8c651f57a82c86eb127131330978bdac5713
magl0 <62085925+magl0@users.noreply.github.com>
2022-11-21 14:25:43 -0800

    Code style change for Cve202226133DetectorWithoutCallbackServerTest.java

--- community/detectors/atlassian_bitbucket_dc_cve_2022_26133/src/test/java/com/google/tsunami/plugins/detectors/rce/cve202226133/Cve202226133DetectorWithoutCallbackServerTest.java
+++ community/detectors/atlassian_bitbucket_dc_cve_2022_26133/src/test/java/com/google/tsunami/plugins/detectors/rce/cve202226133/Cve202226133DetectorWithoutCallbackServerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Google LLC
+ * Copyright 2022 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,106 +15,111 @@
  */
 package com.google.tsunami.plugins.detectors.rce.cve202226133;
 
-import com.google.common.collect.ImmutableList;
-import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
-import com.google.inject.multibindings.OptionalBinder;
-import com.google.tsunami.common.net.http.HttpClientModule;
-import com.google.tsunami.common.time.testing.FakeUtcClock;
-import com.google.tsunami.common.time.testing.FakeUtcClockModule;
-import com.google.tsunami.plugin.payload.testing.FakePayloadGeneratorModule;
-import com.google.tsunami.proto.DetectionReportList;
-import com.google.tsunami.proto.NetworkService;
-import com.google.tsunami.proto.TargetInfo;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-import org.mockito.stubbing.Answer;
-
-import javax.inject.Inject;
-import javax.net.SocketFactory;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.net.Socket;
-import java.time.Instant;
-
 import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import com.google.common.collect.ImmutableList;
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Key;
+import com.google.inject.multibindings.OptionalBinder;
+import com.google.tsunami.common.net.http.HttpClientModule;
+import com.google.tsunami.common.time.testing.FakeUtcClock;
+import com.google.tsunami.common.time.testing.FakeUtcClockModule;
+import com.google.tsunami.plugin.payload.testing.FakePayloadGeneratorModule;
+import com.google.tsunami.plugins.detectors.rce.cve202226133.Cve202226133Detector.SocketFactoryInstance;
+import com.google.tsunami.proto.DetectionReportList;
+import com.google.tsunami.proto.NetworkService;
+import com.google.tsunami.proto.TargetInfo;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.time.Instant;
+import javax.inject.Inject;
+import javax.net.SocketFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.stubbing.Answer;
+
 /** Unit tests for {@link Cve202226133Detector}. */
 @RunWith(JUnit4.class)
 public final class Cve202226133DetectorWithoutCallbackServerTest {
-    private final FakeUtcClock fakeUtcClock =
-        FakeUtcClock.create().setNow(Instant.parse("2022-10-20T00:00:00.00Z"));
-    private final SocketFactory socketFactoryMock = mock(SocketFactory.class);
-    @Inject private Cve202226133Detector detector;
-    private TargetInfo targetInfo;
-    private NetworkService service;
+  private final FakeUtcClock fakeUtcClock =
+      FakeUtcClock.create().setNow(Instant.parse("2022-10-20T00:00:00.00Z"));
+  private final SocketFactory socketFactoryMock = mock(SocketFactory.class);
+  @Inject private Cve202226133Detector detector;
+  private TargetInfo targetInfo;
+  private NetworkService service;
 
-    @Before
-    public void setUp() {
-        Guice.createInjector(
-                new FakeUtcClockModule(fakeUtcClock),
-                new AbstractModule() {
-                    @Override
-                    protected void configure() {
-                        OptionalBinder.newOptionalBinder(binder(), SocketFactory.class)
-                            .setBinding()
-                            .toInstance(socketFactoryMock);
-                    }
-                },
-                new HttpClientModule.Builder().build(),
-                FakePayloadGeneratorModule.builder().build(),
-                new Cve202226133DetectorBootstrapModule())
-            .injectMembers(this);
+  @Before
+  public void setUp() {
+    Guice.createInjector(
+            new FakeUtcClockModule(fakeUtcClock),
+            new AbstractModule() {
+              @Override
+              protected void configure() {
+                OptionalBinder.newOptionalBinder(
+                        binder(), Key.get(SocketFactory.class, SocketFactoryInstance.class))
+                    .setBinding()
+                    .toInstance(socketFactoryMock);
+              }
+            },
+            new HttpClientModule.Builder().build(),
+            FakePayloadGeneratorModule.builder().build(),
+            new Cve202226133DetectorBootstrapModule())
+        .injectMembers(this);
 
-        service = TestHelper.bitbucketClusterService();
+    service = TestHelper.bitbucketClusterService();
 
-        targetInfo = TestHelper.targetInfo();
-    }
+    targetInfo = TestHelper.targetInfo();
+  }
 
-    @Test
-    public void detect_whenVulnerable_returnsVulnerability() throws Exception {
-        Socket socket = mock(Socket.class);
-        when(socketFactoryMock.createSocket(anyString(), anyInt())).thenReturn(socket);
-        when(socket.getOutputStream()).thenReturn(new ByteArrayOutputStream());
-        when(socket.getInputStream()).thenReturn(new ByteArrayInputStream(TestHelper.CLUSTER_NAME))
-            .thenAnswer(
-                (Answer) invocation -> {
-                    Thread.sleep(4100);
-                    throw new IOException();
+  @Test
+  public void detect_whenVulnerable_returnsVulnerability() throws Exception {
+    Socket socket = mock(Socket.class);
+    when(socketFactoryMock.createSocket(anyString(), anyInt())).thenReturn(socket);
+    when(socket.getOutputStream()).thenReturn(new ByteArrayOutputStream());
+    when(socket.getInputStream())
+        .thenReturn(new ByteArrayInputStream(TestHelper.CLUSTER_NAME))
+        .thenAnswer(
+            (Answer)
+                invocation -> {
+                  Thread.sleep(4100);
+                  throw new IOException();
                 });
 
-        when(socket.getOutputStream()).thenReturn(new ByteArrayOutputStream());
+    when(socket.getOutputStream()).thenReturn(new ByteArrayOutputStream());
 
-        DetectionReportList detectionReports = detector.detect(targetInfo, ImmutableList.of(service));
+    DetectionReportList detectionReports = detector.detect(targetInfo, ImmutableList.of(service));
 
-        assertThat(detectionReports.getDetectionReportsList())
-            .containsExactly(TestHelper.buildValidDetectionReport(targetInfo, service, fakeUtcClock));
-    }
+    assertThat(detectionReports.getDetectionReportsList())
+        .containsExactly(TestHelper.buildValidDetectionReport(targetInfo, service, fakeUtcClock));
+  }
 
-
-    @Test
-    public void detect_ifNotVulnerable_doesNotReportVuln() throws Exception {
-        Socket socket = mock(Socket.class);
-        when(socketFactoryMock.createSocket(anyString(), anyInt())).thenReturn(socket);
-        when(socket.getOutputStream()).thenReturn(new ByteArrayOutputStream());
-        when(socket.getInputStream()).thenReturn(new ByteArrayInputStream(TestHelper.CLUSTER_NAME))
-            .thenAnswer(
-                (Answer) invocation -> {
-                    Thread.sleep(100);
-                    throw new IOException();
+  @Test
+  public void detect_ifNotVulnerable_doesNotReportVuln() throws Exception {
+    Socket socket = mock(Socket.class);
+    when(socketFactoryMock.createSocket(anyString(), anyInt())).thenReturn(socket);
+    when(socket.getOutputStream()).thenReturn(new ByteArrayOutputStream());
+    when(socket.getInputStream())
+        .thenReturn(new ByteArrayInputStream(TestHelper.CLUSTER_NAME))
+        .thenAnswer(
+            (Answer)
+                invocation -> {
+                  Thread.sleep(100);
+                  throw new IOException();
                 });
 
-        when(socket.getOutputStream()).thenReturn(new ByteArrayOutputStream());
+    when(socket.getOutputStream()).thenReturn(new ByteArrayOutputStream());
 
-        DetectionReportList detectionReports = detector.detect(targetInfo, ImmutableList.of(service));
+    DetectionReportList detectionReports = detector.detect(targetInfo, ImmutableList.of(service));
 
-        assertThat(detectionReports.getDetectionReportsList()).isEmpty();
-    }
+    assertThat(detectionReports.getDetectionReportsList()).isEmpty();
+  }
 }
