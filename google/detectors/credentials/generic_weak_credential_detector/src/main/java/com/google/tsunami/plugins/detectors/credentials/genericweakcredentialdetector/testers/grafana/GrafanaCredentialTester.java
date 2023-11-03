6791bcbaf97c89cd308812fe3ee9b9d13c92683c
AlessandroCotto <giovanni@doyensec.com>
2023-11-03 15:25:49 +0100

    Add grafanaCredentialTester

---
+++ google/detectors/credentials/generic_weak_credential_detector/src/main/java/com/google/tsunami/plugins/detectors/credentials/genericweakcredentialdetector/testers/grafana/GrafanaCredentialTester.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.tsunami.plugins.detectors.credentials.genericweakcredentialdetector.testers.grafana;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.flogger.GoogleLogger;
+import com.google.tsunami.common.data.NetworkEndpointUtils;
+import com.google.tsunami.common.data.NetworkServiceUtils;
+import com.google.tsunami.common.net.http.HttpClient;
+import com.google.tsunami.common.net.http.HttpHeaders;
+import com.google.tsunami.common.net.http.HttpResponse;
+import com.google.tsunami.plugins.detectors.credentials.genericweakcredentialdetector.provider.TestCredential;
+import com.google.tsunami.plugins.detectors.credentials.genericweakcredentialdetector.tester.CredentialTester;
+import com.google.tsunami.proto.NetworkService;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.tsunami.common.net.http.HttpRequest.get;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+/** Credential tester specifically for grafana. */
+public final class GrafanaCredentialTester extends CredentialTester {
+  private static final GoogleLogger logger = GoogleLogger.forEnclosingClass();
+  private final HttpClient httpClient;
+
+  private static final String GRAFANA_SERVICE =  "grafana";
+  private static final String GRAFANA_PAGE_TITLE =  "grafana";
+
+  @Inject
+  GrafanaCredentialTester(HttpClient httpClient) {
+    this.httpClient = checkNotNull(httpClient);
+  }
+
+  @Override
+  public String name() {
+    return "GrafanaCredentialTester";
+  }
+
+  @Override
+  public String description() {
+    return "Grafana credential tester.";
+  }
+
+  /**
+   * Determines if this tester can accept the {@link NetworkService} based on the name of the service or a custom fingerprint.
+   * The fingerprint is necessary since nmap doesn't recognize a grafana instance correctly.
+   * @param networkService the network service passed by tsunami
+   * @return true if a grafana instance is recognized
+   */
+  @Override
+  public boolean canAccept(NetworkService networkService) {
+
+    String uriAuthority = NetworkEndpointUtils.toUriAuthority(networkService.getNetworkEndpoint());
+
+    boolean canAcceptByNmapReport = NetworkServiceUtils.getWebServiceName(networkService).equals(GRAFANA_SERVICE);
+    boolean canAcceptByCustomFingerprint = false;
+
+    var url = String.format("http://%s/", uriAuthority);
+    try {
+      logger.atInfo().log("probing Grafana home - custom fingerprint phase");
+
+      HttpResponse response = httpClient.send(get(url).withEmptyHeaders().build());
+      canAcceptByCustomFingerprint = response.status().isSuccess()
+              && response
+              .bodyString()
+              .map(GrafanaCredentialTester::bodyContainsGrafanaElements)
+              .orElse(false);
+    } catch (IOException e) {
+      logger.atWarning().withCause(e).log("Unable to query '%s'.", url);
+      return false;
+    }
+
+    return canAcceptByNmapReport || canAcceptByCustomFingerprint;
+  }
+
+  // Checks if the response body contains elements of a grafana page - custom fingerprinting phase
+  private static boolean bodyContainsGrafanaElements(String responseBody) {
+    Document doc = Jsoup.parse(responseBody);
+    String title = doc.title();
+
+    if (title.toLowerCase().contains(GRAFANA_PAGE_TITLE)) {
+      logger.atInfo().log("Grafana instance probably found");
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+  // NOTE: grafana includes a login ratelimit by default in its config file https://github.com/grafana/grafana/blob/main/conf/defaults.ini as follows "disable_brute_force_login_protection = false"
+  // The ratelimit will prevent the plugin from finding potential weak valid credentials.
+  @Override
+  public ImmutableList<TestCredential> testValidCredentials(
+      NetworkService networkService, List<TestCredential> credentials) {
+
+    return credentials.stream()
+            .filter(cred -> isGrafanaAccessible(networkService, cred))
+            .collect(toImmutableList());
+  }
+
+
+
+  private boolean isGrafanaAccessible(NetworkService networkService, TestCredential credential) {
+    var uriAuthority = NetworkEndpointUtils.toUriAuthority(networkService.getNetworkEndpoint());
+    var url = String.format("http://%s/", uriAuthority) + "dashboards";
+    try {
+      logger.atInfo().log(
+              "url: %s, username: %s, password: %s",
+              url, credential.username(), credential.password().orElse(""));
+      HttpResponse response = sendRequestWithCredentials(url, credential);
+
+      return response.status().isSuccess()
+              && response
+              .bodyString()
+              .map(GrafanaCredentialTester::bodyContainsSuccessfulLoginElements)
+              .orElse(false);
+    } catch (IOException e) {
+      logger.atWarning().withCause(e).log("Unable to query '%s'.", url);
+      return false;
+    }
+  }
+
+  private HttpResponse sendRequestWithCredentials(String url, TestCredential credential)
+          throws IOException {
+
+    var headers = HttpHeaders.builder()
+            .addHeader(
+                    "Authorization",
+                    "Basic "
+                            + Base64.getEncoder()
+                            .encodeToString(
+                                    (credential.username() + ":" + credential.password().orElse(""))
+                                            .getBytes(UTF_8)))
+            .build();
+
+    logger.atInfo().log("sending user e pass in headers " + headers + " to urls " + url);
+    return httpClient.send(
+            get(url)
+                    .setHeaders(headers)
+
+                    .build());
+  }
+
+  //Check if the response contains elements in a page after a successful login via Basic Authentication
+   private static boolean bodyContainsSuccessfulLoginElements(String responseBody) {
+
+    // Calling the dashboard path with successful authentication will return a " window.grafanaBootData = {...}" json object
+    // This method looks for the string "isSignedIn":true to check for access. In case of enabled anonymous access the response will contain "isSignedIn":false;
+    // in case of a successful login via Basic Authentication the responseBody will contain "isSignedIn":true
+
+    String successfulLoginInfo = "\"isSignedIn\":true";
+    if (!responseBody.contains(successfulLoginInfo)) {
+      return false;
+    } else {
+      logger.atInfo().log("Successful login in grafana");
+      return true;
+    }
+  }
+
+}
+
---
+++ google/detectors/credentials/generic_weak_credential_detector/src/test/java/com/google/tsunami/plugins/detectors/credentials/genericweakcredentialdetector/testers/grafana/GrafanaCredentialTesterTest.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.tsunami.plugins.detectors.credentials.genericweakcredentialdetector.testers.grafana;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.Resources;
+import com.google.inject.Guice;
+import com.google.tsunami.common.net.db.ConnectionProviderInterface;
+import com.google.tsunami.common.net.http.HttpClientModule;
+import com.google.tsunami.common.net.http.HttpStatus;
+import com.google.tsunami.plugins.detectors.credentials.genericweakcredentialdetector.provider.TestCredential;
+import com.google.tsunami.proto.NetworkService;
+import com.google.tsunami.proto.ServiceContext;
+import com.google.tsunami.proto.Software;
+import com.google.tsunami.proto.WebServiceContext;
+import okhttp3.mockwebserver.Dispatcher;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.Optional;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.tsunami.common.data.NetworkEndpointUtils.forHostnameAndPort;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+
+/** Tests for {@link GrafanaCredentialTester}. */
+@RunWith(JUnit4.class)
+public class GrafanaCredentialTesterTest {
+  @Rule public MockitoRule rule = MockitoJUnit.rule();
+  @Mock private ConnectionProviderInterface mockConnectionProvider;
+  @Mock private Connection mockConnection;
+  @Inject private GrafanaCredentialTester tester;
+  private MockWebServer mockWebServer;
+  private static final TestCredential WEAK_CRED_1 =
+      TestCredential.create("user", Optional.of("1234"));
+  private static final TestCredential WEAK_CRED_2 =
+      TestCredential.create("root", Optional.of("pass"));
+  private static final TestCredential WRONG_CRED_1 =
+      TestCredential.create("wrong", Optional.of("pass"));
+
+  private static final TestCredential EMPTY_CRED = TestCredential.create("", Optional.of(""));
+  private static final String WEAK_CRED_AUTH_1 = "Authorization: Basic dXNlcjoxMjM0";
+  private static final String WEAK_CRED_AUTH_2 = "Authorization: Basic cm9vdDpwYXNz";
+  private static final ServiceContext.Builder grafanaServiceContext =
+      ServiceContext.newBuilder()
+          .setWebServiceContext(
+              WebServiceContext.newBuilder().setSoftware(Software.newBuilder().setName("grafana")));
+
+  @Before
+  public void setup() {
+    mockWebServer = new MockWebServer();
+    Guice.createInjector(new HttpClientModule.Builder().build()).injectMembers(this);
+  }
+
+  @Test
+  public void detect_weakCredentialsExists_returnsWeakCredentials() throws Exception {
+    startMockWebServer(
+        "/dashboards",
+        200,
+        Resources.toString(
+            Resources.getResource(this.getClass(), "testdata/dashboardsPage.html"), UTF_8));
+    NetworkService targetNetworkService =
+        NetworkService.newBuilder()
+            .setNetworkEndpoint(
+                forHostnameAndPort(mockWebServer.getHostName(), mockWebServer.getPort()))
+            .setServiceName("http")
+            .setServiceContext(grafanaServiceContext)
+            .setSoftware(Software.newBuilder().setName("http"))
+            .build();
+
+    assertThat(tester.testValidCredentials(targetNetworkService, ImmutableList.of(WEAK_CRED_1)))
+        .containsExactly(WEAK_CRED_1);
+    mockWebServer.shutdown();
+  }
+
+  @Test
+  public void detect_weakCredentialsExist_returnsAllWeakCredentials() throws Exception {
+    startMockWebServer(
+        "/dashboards",
+        200,
+        Resources.toString(
+            Resources.getResource(this.getClass(), "testdata/dashboardsPage.html"), UTF_8));
+    NetworkService targetNetworkService =
+        NetworkService.newBuilder()
+            .setNetworkEndpoint(
+                forHostnameAndPort(mockWebServer.getHostName(), mockWebServer.getPort()))
+            .setServiceName("http")
+            .setServiceContext(grafanaServiceContext)
+            .build();
+
+    assertThat(
+            tester.testValidCredentials(
+                targetNetworkService, ImmutableList.of(WEAK_CRED_1, WEAK_CRED_2)))
+        .containsExactly(WEAK_CRED_1, WEAK_CRED_2);
+
+    mockWebServer.shutdown();
+  }
+
+
+  @Test
+  public void detect_noWeakCredentials_returnsNoCredentials() throws Exception {
+    startMockWebServer("/dashboard", 200,
+            Resources.toString(
+                    Resources.getResource(this.getClass(), "testdata/dashboardsPage.html"), UTF_8)
+    );
+    NetworkService targetNetworkService =
+        NetworkService.newBuilder()
+            .setNetworkEndpoint(
+                forHostnameAndPort(mockWebServer.getHostName(), mockWebServer.getPort()))
+            .setServiceName("http")
+            .setServiceContext(grafanaServiceContext)
+            .build();
+
+    assertThat(tester.testValidCredentials(targetNetworkService, ImmutableList.of(WRONG_CRED_1)))
+        .isEmpty();
+
+    mockWebServer.shutdown();
+  }
+
+
+
+  private void startMockWebServer(String url, int responseCode, String response)
+      throws IOException {
+    mockWebServer.setDispatcher(new RespondWithDashboardsPageDispatcher(response));
+    mockWebServer.start();
+    mockWebServer.url(url);
+  }
+
+  static final class RespondWithDashboardsPageDispatcher extends Dispatcher {
+    private final String dashboardsPage;
+
+    RespondWithDashboardsPageDispatcher(String dashboardPageResponse) {
+      this.dashboardsPage = checkNotNull(dashboardPageResponse);
+    }
+
+    @Override
+    public MockResponse dispatch(RecordedRequest recordedRequest) {
+      if (recordedRequest.getPath().startsWith("/dashboards")
+          && (recordedRequest.getHeaders().toString().contains(WEAK_CRED_AUTH_1)
+              || recordedRequest.getHeaders().toString().contains(WEAK_CRED_AUTH_2))
+      ) {
+        return new MockResponse().setResponseCode(HttpStatus.OK.code()).setBody(dashboardsPage);
+      }
+      return new MockResponse().setResponseCode(HttpStatus.UNAUTHORIZED.code());
+    }
+  }
+}
+
+
