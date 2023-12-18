059a8e45c5236e13172d3be40ed0e39c42f21d2e
Nghi Tran <56736094+nttran8@users.noreply.github.com>
2023-12-18 12:13:39 -0800

    Linter of using Default ZoneOffset Time.java

--- community/detectors/minio_cve_2023_28432/src/main/java/com/google/tsunami/plugins/cves/cve202328432/minio/Time.java
+++ community/detectors/minio_cve_2023_28432/src/main/java/com/google/tsunami/plugins/cves/cve202328432/minio/Time.java
@@ -16,29 +16,30 @@
 package com.google.tsunami.plugins.cves.cve202328432.minio;
 
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 /** Time formatters for S3 APIs. */
 public class Time {
-    public static final ZoneId UTC = ZoneId.of("Z");
+  public static final ZoneId UTC = ZoneOffset.UTC;
 
-    public static final DateTimeFormatter AMZ_DATE_FORMAT =
-            DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss'Z'", Locale.US).withZone(UTC);
+  public static final DateTimeFormatter AMZ_DATE_FORMAT =
+      DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss'Z'", Locale.US).withZone(UTC);
 
-    public static final DateTimeFormatter RESPONSE_DATE_FORMAT =
-            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH':'mm':'ss'.'SSS'Z'", Locale.US).withZone(UTC);
+  public static final DateTimeFormatter RESPONSE_DATE_FORMAT =
+      DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH':'mm':'ss'.'SSS'Z'", Locale.US).withZone(UTC);
 
-    // Formatted string is convertible to LocalDate only, not to LocalDateTime or ZonedDateTime.
-    // Below example shows how to use this to get ZonedDateTime.
-    // LocalDate.parse("20200225", SIGNER_DATE_FORMAT).atStartOfDay(UTC);
-    public static final DateTimeFormatter SIGNER_DATE_FORMAT =
-            DateTimeFormatter.ofPattern("yyyyMMdd", Locale.US).withZone(UTC);
+  // Formatted string is convertible to LocalDate only, not to LocalDateTime or ZonedDateTime.
+  // Below example shows how to use this to get ZonedDateTime.
+  // LocalDate.parse("20200225", SIGNER_DATE_FORMAT).atStartOfDay(UTC);
+  public static final DateTimeFormatter SIGNER_DATE_FORMAT =
+      DateTimeFormatter.ofPattern("yyyyMMdd", Locale.US).withZone(UTC);
 
-    public static final DateTimeFormatter HTTP_HEADER_DATE_FORMAT =
-            DateTimeFormatter.ofPattern("EEE',' dd MMM yyyy HH':'mm':'ss 'GMT'", Locale.US).withZone(UTC);
+  public static final DateTimeFormatter HTTP_HEADER_DATE_FORMAT =
+      DateTimeFormatter.ofPattern("EEE',' dd MMM yyyy HH':'mm':'ss 'GMT'", Locale.US).withZone(UTC);
 
-    public static final DateTimeFormatter EXPIRATION_DATE_FORMAT = RESPONSE_DATE_FORMAT;
+  public static final DateTimeFormatter EXPIRATION_DATE_FORMAT = RESPONSE_DATE_FORMAT;
 
-    private Time() {}
+  private Time() {}
 }
