edfb05abda793cb4cb1bfc41c64fea1e23573098
Andrew Konstantinov <105389353+execveat@users.noreply.github.com>
2023-12-20 15:54:06 +0100

    Add copyright notices and a header explaining the TorchServe script

--- doyensec/detectors/rce/torchserve/src/main/java/com/google/tsunami/plugins/detectors/rce/torchserve/TorchServeManagementApiDetector.java
+++ doyensec/detectors/rce/torchserve/src/main/java/com/google/tsunami/plugins/detectors/rce/torchserve/TorchServeManagementApiDetector.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.tsunami.plugins.detectors.rce.torchserve;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -25,7 +40,7 @@ public final class TorchServeManagementApiDetector implements VulnDetector {
   private final TorchServeExploiter torchServeExploiter;
   private static final GoogleLogger logger = GoogleLogger.forEnclosingClass();
 
-  static final String REPORT_PUBLISHER = "TSUNAMI_COMMUNITY";
+  static final String REPORT_PUBLISHER = "DOYENSEC";
   static final String REPORT_ID = "TORCHSERVE_MANAGEMENT_API_RCE";
   static final String REPORT_TITLE = "TorchServe Management API Remote Code Execution";
   static final String REPORT_RECOMMENDATION =
--- doyensec/detectors/rce/torchserve/src/main/resources/model/model.py
+++ doyensec/detectors/rce/torchserve/src/main/resources/model/model.py
@@ -1,3 +1,40 @@
+#!/usr/bin/env python
+
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This Python script is part of a vulnerability detection plugin for Google's Tsunami Security Scanner.
+The plugin is designed to assess the Remote Code Execution (RCE) capability of systems by deploying
+this script as a model in TorchServe. It handles specific HTTP request headers to simulate various
+actions without creating additional attack surface. The script's behaviors include:
+
+- Calculating an MD5 hash for the 'tsunami-execute' header value to simulate command execution.
+- Sending a GET request to a specified URL in the 'tsunami-callback' header to validate data exfiltration.
+- Collecting basic system information in response to the 'tsunami-info' header for aiding vulnerability mitigation.
+- Logging the value of the 'tsunami-log' header to the container's standard output.
+
+This script is a part of an automated testing process and does not perform any malicious activities.
+In case of unexpected termination of the Tsunami plugin, this script may remain on the system. To verify
+its origin and purpose, please refer to the following repositories:
+
+- Tsunami Security Scanner: https://github.com/google/tsunami-security-scanner
+- Tsunami RCE Plugin for TorchServe: https://github.com/google/tsunami-security-scanner-plugins/tree/master/doyensec/detectors/rce/torchserve
+
+The plugin attempts to clean up by removing the model post-execution, with the primary output being a log entry.
+"""
+
 import hashlib
 import urllib.request
 import platform
@@ -18,16 +55,16 @@ def handle(data, context):
 
     response = data[0]
     if "tsunami-execute" in headers:
-        # Calculate the MD5 hash of the headers["execute"] value
+        # Simulate command execution by calculating an MD5 hash of the headers value
         response = hashlib.md5(headers["tsunami-execute"].encode()).hexdigest()
     elif "tsunami-callback" in headers:
-        # Create a GET request to the headers["callback"] URL through standard library
+        # Validates data exfiltration by sending a GET request to the specified URL
         try:
             urllib.request.urlopen(headers["tsunami-callback"])
         except:
             pass
     elif "tsunami-info" in headers:
-        # Collect some basic system info to simplify vulnerability mitigation
+        # Collects basic system info to simplify vulnerability mitigation
         info = {
             "platform": platform.platform(),
             "python": platform.python_version(),
@@ -36,7 +73,7 @@ def handle(data, context):
         response = json.dumps(info)
 
     elif "tsunami-log" in headers:
-        # Log the headers["log"] value to the container's stdout
+        # Logs the value of the 'tsunami-log' header to the container's standard output
         print(headers["tsunami-log"])
 
     return produce_conformant_output(response, len(data))
