8d2170e5b765af3f32aa2e22f2460284802514f1
Furkan Göksel <furkan.goksel27@gmail.com>
2024-03-28 09:07:07 -0700

    Copybara import of the project:
    --
    1f23a1c6a1c81aa3f47ac895a234adac7232c39f by Furkan Göksel <furkan.goksel27@gmail.com>:
    
    Add CVE-2023-6014
    
    --
    ea3ff37ff3e24d4469951b7bb74fa3c37f45f450 by Furkan Göksel <furkan.goksel27@gmail.com>:
    
    Update Cve20236014VulnDetectorTest.java
    --
    2ea3159e4d3215e85852636ffaa55f8394545cba by Furkan Göksel <furkan.goksel27@gmail.com>:
    
    Update community/detectors/mlflow_cve_2023_6014/src/main/java/com/google/tsunami/plugins/detectors/cves/cve20236014/Cve20236014VulnDetector.java
    
    Co-authored-by: Viviana Sutedjo <37225049+vsutedjo@users.noreply.github.com>
    --
    95655750dd31f8165438ae250d8f442b8c175ecd by Furkan Göksel <furkan.goksel27@gmail.com>:
    
    Update Cve20236014VulnDetector.java
    --
    7573a6772068da9d9fc744752b1087b3d0c701ea by Furkan Göksel <furkan.goksel27@gmail.com>:
    
    Update community/detectors/mlflow_cve_2023_6014/src/main/java/com/google/tsunami/plugins/detectors/cves/cve20236014/Cve20236014VulnDetector.java
    
    Co-authored-by: Viviana Sutedjo <37225049+vsutedjo@users.noreply.github.com>
    COPYBARA_INTEGRATE_REVIEW=https://github.com/google/tsunami-security-scanner-plugins/pull/438 from frkngksl:mlflowRce 719fde05365f8dc7127d91e0b27c653c42af9c11
    PiperOrigin-RevId: 619951949
    Change-Id: I176ae60e640fdfb1f39d2674c5840c198511d28a

--- community/detectors/mlflow_cve_2023_6014/src/main/java/com/google/tsunami/plugins/detectors/cves/cve20236014/Cve20236014VulnDetector.java
+++ community/detectors/mlflow_cve_2023_6014/src/main/java/com/google/tsunami/plugins/detectors/cves/cve20236014/Cve20236014VulnDetector.java
@@ -126,6 +126,7 @@ public final class Cve20236014VulnDetector implements VulnDetector {
     try {
       String randomUsername = GenerateRandomString(10);
       String randomPassword = GenerateRandomString(10);
+
       logger.atInfo().log("Attempting to create a user (%s: %s)", randomUsername, randomPassword);
       String stringPayload = "username=" + randomUsername + "&password=" + randomPassword;
       ByteString bytePayload = ByteString.copyFromUtf8(stringPayload);
