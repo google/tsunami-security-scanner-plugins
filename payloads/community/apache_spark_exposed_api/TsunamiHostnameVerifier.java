6a3bb96e61c7d068abd19cc19cac781d2b985a2a
Timo Mueller <mueller@mogwailabs.de>
2024-04-08 23:02:36 +0200

    remove payload as we merge this in #461

--- payloads/community/apache_spark_exposed_api/TsunamiHostnameVerifier.java
+++
@@ -1,9 +0,0 @@
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLSession;
-
-public class TsunamiHostnameVerifier implements HostnameVerifier {
-	public boolean verify(String arg0, SSLSession arg1) {
-		return true;
-	}
-};
-
