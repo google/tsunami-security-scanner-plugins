cff965f99abaf46a8ac8a2950ccc541e92f8917d
am0o0 <77095239+am0o0@users.noreply.github.com>
2024-04-27 00:06:28 +0400

    remove redundant space in test

--- community/detectors/apache_airflow_cve_2020_17526/src/test/java/com/google/tsunami/plugins/cve202017526/Cve202017526DetectorTest.java
+++ community/detectors/apache_airflow_cve_2020_17526/src/test/java/com/google/tsunami/plugins/cve202017526/Cve202017526DetectorTest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.google.tsunami.plugins.cve202017526;
 
 import static com.google.common.truth.extensions.proto.ProtoTruth.assertThat;
