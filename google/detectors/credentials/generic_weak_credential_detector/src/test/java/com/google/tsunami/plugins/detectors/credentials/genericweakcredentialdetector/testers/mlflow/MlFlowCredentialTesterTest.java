478abbff3c283eb2da7bed52a4d41104b13a4c28
lanced00m <lanced00m00@gmail.com>
2024-04-27 15:09:20 +0400

    change create new user method to get user info method, fix tests accordingly

--- google/detectors/credentials/generic_weak_credential_detector/src/test/java/com/google/tsunami/plugins/detectors/credentials/genericweakcredentialdetector/testers/mlflow/MlFlowCredentialTesterTest.java
+++ google/detectors/credentials/generic_weak_credential_detector/src/test/java/com/google/tsunami/plugins/detectors/credentials/genericweakcredentialdetector/testers/mlflow/MlFlowCredentialTesterTest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.google.tsunami.plugins.detectors.credentials.genericweakcredentialdetector.testers.mlflow;
 
 import com.google.common.collect.ImmutableList;
@@ -34,166 +35,167 @@ import org.junit.runners.JUnit4;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
-
 import javax.inject.Inject;
 import java.io.IOException;
 import java.sql.Connection;
 import java.util.Objects;
 import java.util.Optional;
-
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.tsunami.common.data.NetworkEndpointUtils.forHostnameAndPort;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
-/**
- * Tests for {@link MlFlowCredentialTester}.
- */
+/** Tests for {@link MlFlowCredentialTester}. */
 @RunWith(JUnit4.class)
 public class MlFlowCredentialTesterTest {
-    @Rule
-    public MockitoRule rule = MockitoJUnit.rule();
-    @Mock
-    private ConnectionProviderInterface mockConnectionProvider;
-    @Mock
-    private Connection mockConnection;
-    @Inject
-    private MlFlowCredentialTester tester;
-    private MockWebServer mockWebServer;
-    private static final TestCredential WEAK_CRED_1 =
-            TestCredential.create("admin", Optional.of("password"));
-    private static final TestCredential WEAK_CRED_2 =
-            TestCredential.create("username", Optional.of("password"));
-    private static final TestCredential WRONG_CRED_1 =
-            TestCredential.create("wrong", Optional.of("wrong"));
+  @Rule public MockitoRule rule = MockitoJUnit.rule();
+  @Mock private ConnectionProviderInterface mockConnectionProvider;
+  @Mock private Connection mockConnection;
+  @Inject private MlFlowCredentialTester tester;
+  private MockWebServer mockWebServer;
+  private static final TestCredential WEAK_CRED_1 =
+      TestCredential.create("admin", Optional.of("password"));
+  private static final TestCredential WEAK_CRED_2 =
+      TestCredential.create("username", Optional.of("password"));
+  private static final TestCredential WRONG_CRED_1 =
+      TestCredential.create("wrong", Optional.of("wrong"));
 
-    private static final String WEAK_CRED_AUTH_1 = "basic dXNlcm5hbWU6cGFzc3dvcmQ=";
-    private static final String WEAK_CRED_AUTH_2 = "basic YWRtaW46cGFzc3dvcmQ=";
-    private static final GoogleLogger logger = GoogleLogger.forEnclosingClass();
+  private static final String WEAK_CRED_AUTH_1 = "basic dXNlcm5hbWU6cGFzc3dvcmQ=";
+  private static final String WEAK_CRED_AUTH_2 = "basic YWRtaW46cGFzc3dvcmQ=";
+  private static final GoogleLogger logger = GoogleLogger.forEnclosingClass();
 
-    @Before
-    public void setup() {
-        mockWebServer = new MockWebServer();
-        Guice.createInjector(new HttpClientModule.Builder().build()).injectMembers(this);
-    }
+  @Before
+  public void setup() {
+    mockWebServer = new MockWebServer();
+    Guice.createInjector(new HttpClientModule.Builder().build()).injectMembers(this);
+  }
 
-    @Test
-    public void detect_weakCredentialsExists_returnsWeakCredentials() throws Exception {
-        startMockWebServer();
-        NetworkService targetNetworkService =
-                NetworkService.newBuilder()
-                        .setNetworkEndpoint(
-                                forHostnameAndPort(mockWebServer.getHostName(), mockWebServer.getPort()))
-                        .setServiceName("http")
-                        .build();
+  @Test
+  public void detect_weakCredentialsExists_returnsWeakCredentials() throws Exception {
+    startMockWebServer();
+    NetworkService targetNetworkService =
+        NetworkService.newBuilder()
+            .setNetworkEndpoint(
+                forHostnameAndPort(mockWebServer.getHostName(), mockWebServer.getPort()))
+            .setServiceName("http")
+            .build();
 
-        assertThat(tester.testValidCredentials(targetNetworkService, ImmutableList.of(WEAK_CRED_1)))
-                .containsExactly(WEAK_CRED_1);
-        mockWebServer.shutdown();
-    }
+    assertThat(tester.testValidCredentials(targetNetworkService, ImmutableList.of(WEAK_CRED_1)))
+        .containsExactly(WEAK_CRED_1);
+    mockWebServer.shutdown();
+  }
 
-    @Test
-    public void detect_weakCredentialsExist_returnsFirstWeakCredentials() throws Exception {
-        startMockWebServer();
-        NetworkService targetNetworkService =
-                NetworkService.newBuilder()
-                        .setNetworkEndpoint(
-                                forHostnameAndPort(mockWebServer.getHostName(), mockWebServer.getPort()))
-                        .setServiceName("http")
-                        .build();
+  @Test
+  public void detect_weakCredentialsExist_returnsFirstWeakCredentials() throws Exception {
+    startMockWebServer();
+    NetworkService targetNetworkService =
+        NetworkService.newBuilder()
+            .setNetworkEndpoint(
+                forHostnameAndPort(mockWebServer.getHostName(), mockWebServer.getPort()))
+            .setServiceName("http")
+            .build();
 
-        assertThat(
-                tester.testValidCredentials(
-                        targetNetworkService, ImmutableList.of(WEAK_CRED_1, WEAK_CRED_2)))
-                .containsExactly(WEAK_CRED_1);
-    }
+    assertThat(
+            tester.testValidCredentials(
+                targetNetworkService, ImmutableList.of(WEAK_CRED_1, WEAK_CRED_2)))
+        .containsExactly(WEAK_CRED_1);
+  }
 
-    @Test
-    public void detect_canAccept() throws Exception {
-        startMockWebServer();
-        NetworkService targetNetworkService =
-                NetworkService.newBuilder()
-                        .setNetworkEndpoint(
-                                forHostnameAndPort(mockWebServer.getHostName(), mockWebServer.getPort()))
-                        .setServiceName("http")
-                        .build();
+  @Test
+  public void detect_canAccept() throws Exception {
+    startMockWebServer();
+    NetworkService targetNetworkService =
+        NetworkService.newBuilder()
+            .setNetworkEndpoint(
+                forHostnameAndPort(mockWebServer.getHostName(), mockWebServer.getPort()))
+            .setServiceName("http")
+            .build();
 
-        assertThat(tester.canAccept(targetNetworkService)).isTrue();
-    }
+    assertThat(tester.canAccept(targetNetworkService)).isTrue();
+  }
 
-    @Test
-    public void detect_weakCredentialsExistAndMlflowInForeignLanguage_returnsFirstWeakCredentials()
-            throws Exception {
-        startMockWebServer();
-        NetworkService targetNetworkService =
-                NetworkService.newBuilder()
-                        .setNetworkEndpoint(
-                                forHostnameAndPort(mockWebServer.getHostName(), mockWebServer.getPort()))
-                        .setServiceName("http")
-                        .build();
+  @Test
+  public void detect_weakCredentialsExistAndMlflowInForeignLanguage_returnsFirstWeakCredentials()
+      throws Exception {
+    startMockWebServer();
+    NetworkService targetNetworkService =
+        NetworkService.newBuilder()
+            .setNetworkEndpoint(
+                forHostnameAndPort(mockWebServer.getHostName(), mockWebServer.getPort()))
+            .setServiceName("http")
+            .build();
 
-        assertThat(
-                tester.testValidCredentials(
-                        targetNetworkService, ImmutableList.of(WEAK_CRED_1, WEAK_CRED_2)))
-                .containsExactly(WEAK_CRED_1);
-    }
+    assertThat(
+            tester.testValidCredentials(
+                targetNetworkService, ImmutableList.of(WEAK_CRED_1, WEAK_CRED_2)))
+        .containsExactly(WEAK_CRED_1);
+  }
 
-    @Test
-    public void detect_noWeakCredentials_returnsNoCredentials() throws Exception {
-        startMockWebServer();
-        NetworkService targetNetworkService =
-                NetworkService.newBuilder()
-                        .setNetworkEndpoint(
-                                forHostnameAndPort(mockWebServer.getHostName(), mockWebServer.getPort()))
-                        .setServiceName("http")
-                        .build();
-        assertThat(tester.testValidCredentials(targetNetworkService, ImmutableList.of(WRONG_CRED_1)))
-                .isEmpty();
-    }
+  @Test
+  public void detect_noWeakCredentials_returnsNoCredentials() throws Exception {
+    startMockWebServer();
+    NetworkService targetNetworkService =
+        NetworkService.newBuilder()
+            .setNetworkEndpoint(
+                forHostnameAndPort(mockWebServer.getHostName(), mockWebServer.getPort()))
+            .setServiceName("http")
+            .build();
+    assertThat(tester.testValidCredentials(targetNetworkService, ImmutableList.of(WRONG_CRED_1)))
+        .isEmpty();
+  }
 
-    @Test
-    public void detect_nonMlflowService_skips() throws Exception {
-        when(mockConnectionProvider.getConnection(any(), any(), any())).thenReturn(mockConnection);
-        NetworkService targetNetworkService =
-                NetworkService.newBuilder()
-                        .setNetworkEndpoint(forHostnameAndPort("example.com", 8080))
-                        .setServiceName("http")
-                        .build();
+  @Test
+  public void detect_nonMlflowService_skips() throws Exception {
+    when(mockConnectionProvider.getConnection(any(), any(), any())).thenReturn(mockConnection);
+    NetworkService targetNetworkService =
+        NetworkService.newBuilder()
+            .setNetworkEndpoint(forHostnameAndPort("example.com", 8080))
+            .setServiceName("http")
+            .build();
 
-        assertThat(tester.testValidCredentials(targetNetworkService, ImmutableList.of(WEAK_CRED_1)))
-                .isEmpty();
-        verifyNoInteractions(mockConnectionProvider);
-    }
+    assertThat(tester.testValidCredentials(targetNetworkService, ImmutableList.of(WEAK_CRED_1)))
+        .isEmpty();
+    verifyNoInteractions(mockConnectionProvider);
+  }
 
-    private void startMockWebServer()
-            throws IOException {
-        final Dispatcher dispatcher = new Dispatcher() {
-            final MockResponse unauthorizedResponse = new MockResponse().setResponseCode(401).setBody("You are not authenticated. "
-                    + "Please see https://www.mlflow.org/docs/latest/auth/index.html#authenticating-to-mlflow "
-                    + "on how to authenticate");
+  private void startMockWebServer() throws IOException {
+    final Dispatcher dispatcher =
+        new Dispatcher() {
+          final MockResponse unauthorizedResponse =
+              new MockResponse()
+                  .setResponseCode(401)
+                  .setBody(
+                      "You are not authenticated. "
+                          + "Please see https://www.mlflow.org/docs/latest/auth/index.html#authenticating-to-mlflow "
+                          + "on how to authenticate");
 
-            @Override
-            public MockResponse dispatch(RecordedRequest request) {
-                String authorizationHeader = request.getHeaders().get("Authorization");
-                if (authorizationHeader == null) {
-                    return unauthorizedResponse;
-                }
-                if (Objects.equals(request.getPath(), "/api/2.0/mlflow/users/create") && Objects.equals(request.getMethod(), "POST")) {
-                    boolean isDefaultCredentials = authorizationHeader.equals(WEAK_CRED_AUTH_1) || authorizationHeader.equals(WEAK_CRED_AUTH_2);
-                    if (isDefaultCredentials) {
-                        return new MockResponse().setResponseCode(200)
-                                .setBody("{\"user\":{\"experiment_permissions\":[],\"id\":4,\"is_admin\":false,\"registered_model_permissions\":[],\n" +
-                                        "     \"username\":\"googleTsunamiSecurityScanner\"}}");
-                    } else {
-                        return unauthorizedResponse;
-                    }
-                }
-                return new MockResponse().setResponseCode(404);
+          @Override
+          public MockResponse dispatch(RecordedRequest request) {
+            String authorizationHeader = request.getHeaders().get("Authorization");
+            if (authorizationHeader == null) {
+              return unauthorizedResponse;
             }
+            if (request.getPath().matches("/api/2.0/mlflow/users/get\\?.*")
+                && Objects.equals(request.getMethod(), "GET")) {
+              boolean isDefaultCredentials =
+                  authorizationHeader.equals(WEAK_CRED_AUTH_1)
+                      || authorizationHeader.equals(WEAK_CRED_AUTH_2);
+              if (isDefaultCredentials) {
+                return new MockResponse()
+                    .setResponseCode(200)
+                    .setBody(
+                        "{\"user\":{\"experiment_permissions\":[],\"id\":1,\"is_admin\":true,\"registered_model_permissions\":[],"
+                            + "\"username\":\"admin\"}}");
+              } else {
+                return unauthorizedResponse;
+              }
+            }
+            return new MockResponse().setResponseCode(404);
+          }
         };
-        mockWebServer.setDispatcher(dispatcher);
-        mockWebServer.start();
-        mockWebServer.url("/");
-    }
+    mockWebServer.setDispatcher(dispatcher);
+    mockWebServer.start();
+    mockWebServer.url("/");
+  }
 }
