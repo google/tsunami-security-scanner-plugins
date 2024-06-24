8b78c50c00ea82e215a15261de93d52dfd117026
Raul <raul@doyensec.om>
2024-06-24 13:57:11 +0200

    Added License

--- google/detectors/credentials/generic_weak_credential_detector/src/main/java/com/google/tsunami/plugins/detectors/credentials/genericweakcredentialdetector/testers/tomcat/TomcatAjpCredentialTester.java
+++ google/detectors/credentials/generic_weak_credential_detector/src/main/java/com/google/tsunami/plugins/detectors/credentials/genericweakcredentialdetector/testers/tomcat/TomcatAjpCredentialTester.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.tsunami.plugins.detectors.credentials.genericweakcredentialdetector.testers.tomcat;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -94,7 +110,7 @@ public final class TomcatAjpCredentialTester extends CredentialTester {
 
       byte[] response = sendAndReceive(host, port, request.getBytes());
       AjpMessage responseMessage = AjpReader.parseMessage(response);
-      
+
       return headersContainsSuccessfulLoginElements(responseMessage);
     } catch (IOException e) {
       logger.atWarning().withCause(e).log("Unable to query '%s'.", url);
