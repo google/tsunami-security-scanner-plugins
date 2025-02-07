6b79039cd357add78c5e2c05f3fd0a8dbe79c56d
lokiuox <saviosisco@gmail.com>
2025-02-07 17:31:28 +0100

    Fixed tests

--- community/detectors/spring_framework_cve_2022_22965/src/test/java/com/google/tsunami/plugins/detectors/spring4shell/SpringCve202222965DetectorTest.java
+++ community/detectors/spring_framework_cve_2022_22965/src/test/java/com/google/tsunami/plugins/detectors/spring4shell/SpringCve202222965DetectorTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Google LLC
+ * Copyright 2025 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,19 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.tsunami.plugins.detectors.spring;
+package com.google.tsunami.plugins.detectors.spring4shell;
 
 import static com.google.common.truth.extensions.proto.ProtoTruth.assertThat;
 import static com.google.tsunami.common.data.NetworkEndpointUtils.forHostname;
 import static com.google.tsunami.common.data.NetworkEndpointUtils.forHostnameAndPort;
+import static com.google.tsunami.plugins.detectors.spring4shell.Annotations.DelayBetweenRequests;
+import static com.google.tsunami.plugins.detectors.spring4shell.SpringCve202222965Detector.JSP_FILENAME_PREFIX;
 
 import com.google.common.collect.ImmutableList;
 import com.google.inject.Guice;
+import com.google.inject.testing.fieldbinder.Bind;
+import com.google.inject.testing.fieldbinder.BoundFieldModule;
+import com.google.inject.util.Modules;
 import com.google.protobuf.util.Timestamps;
 import com.google.tsunami.common.net.http.HttpClientModule;
 import com.google.tsunami.common.net.http.HttpStatus;
 import com.google.tsunami.common.time.testing.FakeUtcClock;
 import com.google.tsunami.common.time.testing.FakeUtcClockModule;
+import com.google.tsunami.plugin.payload.testing.FakePayloadGeneratorModule;
 import com.google.tsunami.proto.DetectionReport;
 import com.google.tsunami.proto.DetectionReportList;
 import com.google.tsunami.proto.DetectionStatus;
@@ -34,7 +40,9 @@ import com.google.tsunami.proto.Software;
 import com.google.tsunami.proto.TargetInfo;
 import com.google.tsunami.proto.TransportProtocol;
 import java.io.IOException;
+import java.security.SecureRandom;
 import java.time.Instant;
+import java.util.Arrays;
 import javax.inject.Inject;
 import okhttp3.mockwebserver.Dispatcher;
 import okhttp3.mockwebserver.MockResponse;
@@ -53,17 +61,37 @@ public final class SpringCve202222965DetectorTest {
   private final FakeUtcClock fakeUtcClock =
       FakeUtcClock.create().setNow(Instant.parse("2020-01-01T00:00:00.00Z"));
 
+  private final String fakeJspPath = "/" + JSP_FILENAME_PREFIX + fakeUtcClock.millis() + ".jsp";
+
+  @Bind(lazy = true)
+  @DelayBetweenRequests
+  private final int delayBetweenRequests = 0;
+
   @Inject private SpringCve202222965Detector detector;
 
   private MockWebServer mockWebServer;
+  private final SecureRandom testSecureRandom =
+          new SecureRandom() {
+            @Override
+            public void nextBytes(byte[] bytes) {
+              Arrays.fill(bytes, (byte) 0xFF);
+            }
+          };
+
+  private final static String MOCK_PAYLOAD_EXECUTION = "TSUNAMI_PAYLOAD_STARTffffffffffffffffTSUNAMI_PAYLOAD_END";
 
   @Before
   public void setUp() {
     mockWebServer = new MockWebServer();
     Guice.createInjector(
             new FakeUtcClockModule(fakeUtcClock),
-            new SpringCve202222965DetectorBootstrapModule(),
-            new HttpClientModule.Builder().build())
+            FakePayloadGeneratorModule.builder()
+                    .setCallbackServer(null)
+                    .setSecureRng(testSecureRandom)
+                    .build(),
+            new HttpClientModule.Builder().build(),
+            Modules.override(new SpringCve202222965DetectorBootstrapModule())
+                    .with(BoundFieldModule.of(this)))
         .injectMembers(this);
   }
 
@@ -74,7 +102,7 @@ public final class SpringCve202222965DetectorTest {
 
   @Test
   public void detect_whenVulnerable_returnsVulnerability() throws IOException {
-    mockWebServer.setDispatcher(new VulnerabilityEndpointDispatcher());
+    mockWebServer.setDispatcher(new VulnerabilityEndpointDispatcher(this.fakeJspPath, MOCK_PAYLOAD_EXECUTION));
     mockWebServer.start();
     NetworkService service =
         NetworkService.newBuilder()
@@ -125,6 +153,33 @@ public final class SpringCve202222965DetectorTest {
     assertThat(detectionReports.getDetectionReportsList()).isEmpty();
   }
 
+  @Test
+  public void detect_whenFalsePositive_returnsNoVulnerability() throws IOException {
+    /*
+    We simulate a false positive by returning an incorrect response in the
+    (supposedly) uploaded JSP page.
+     */
+    mockWebServer.setDispatcher(new VulnerabilityEndpointDispatcher(this.fakeJspPath, "This is not the page you're looking for."));
+    mockWebServer.start();
+    NetworkService service =
+            NetworkService.newBuilder()
+                    .setNetworkEndpoint(
+                            forHostnameAndPort(mockWebServer.getHostName(), mockWebServer.getPort()))
+                    .setTransportProtocol(TransportProtocol.TCP)
+                    .setSoftware(Software.newBuilder().setName("http"))
+                    .setServiceName("http")
+                    .build();
+    TargetInfo targetInfo =
+            TargetInfo.newBuilder()
+                    .addNetworkEndpoints(forHostname(mockWebServer.getHostName()))
+                    .build();
+
+    DetectionReportList detectionReports = detector.detect(targetInfo, ImmutableList.of(service));
+
+    assertThat(detectionReports.getDetectionReportsList()).isEmpty();
+  }
+
+
   static final class SafeEndpointDispatcher extends Dispatcher {
 
     @Override
@@ -134,18 +189,31 @@ public final class SpringCve202222965DetectorTest {
   }
 
   static final class VulnerabilityEndpointDispatcher extends Dispatcher {
+    private final String jspPath;
+    private final String jspResponse;
+    VulnerabilityEndpointDispatcher(String jspPath, String jspResponse) {
+      this.jspPath = jspPath;
+      this.jspResponse = jspResponse;
+    }
 
     @Override
     public MockResponse dispatch(RecordedRequest recordedRequest) {
-      if ("/?class.module.classLoader.DefaultAssertionStatus=1".equals(recordedRequest.getPath())) {
+      String path = recordedRequest.getPath();
+      if (path.equals("/?class.module.classLoader.DefaultAssertionStatus=1")) {
+        // Handle preliminary check 1
         return new MockResponse().setResponseCode(HttpStatus.OK.code());
-      }
-      if ("/?class.module.classLoader.DefaultAssertionStatus=2".equals(recordedRequest.getPath())) {
+      } else if (path.equals("/?class.module.classLoader.DefaultAssertionStatus=2")) {
+        // Handle preliminary check 2
         return new MockResponse().setResponseCode(HttpStatus.BAD_REQUEST.code());
+      } else if (path.startsWith(jspPath)) {
+        // Handle requests to the uploaded
+        return new MockResponse().setResponseCode(HttpStatus.OK.code()).setBody(jspResponse);
+      } else if (path.startsWith("/?")) {
+        // Handle requests during JSP upload step
+        return new MockResponse().setResponseCode(HttpStatus.OK.code());
+      } else {
+        return new MockResponse().setResponseCode(HttpStatus.NOT_FOUND.code());
       }
-      return new MockResponse()
-          .setResponseCode(HttpStatus.OK.code())
-          .setBody("<p><href=\"http://127.0.0.1:8889/\"></p>");
     }
   }
 }
