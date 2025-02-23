d9685f08d5a0d7bdb902c2802a4f12b4c9f06c74
lanced00m <lanced00m00@gmail.com>
2025-02-23 04:50:10 -0500

    v1 of uptrain API RCE

---
+++ community/detectors/uptrain_exposed_api/src/main/java/com/google/tsunami/plugins/detectors/rce/UptrainExposedApiDetectorBootstrapModule.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.tsunami.plugins.detectors.rce;
+
+import com.google.inject.Provides;
+import com.google.tsunami.plugin.PluginBootstrapModule;
+import com.google.tsunami.plugins.detectors.rce.UptrainExposedApiDetectorAnnotations.UptrainExposedApiOobSleepDuration;
+
+/**
+ * An Exposed Uptrain API Server Detector Guice module that bootstraps the {@link
+ * UptrainExposedApiDetector}.
+ */
+public final class UptrainExposedApiDetectorBootstrapModule extends PluginBootstrapModule {
+
+  @Override
+  protected void configurePlugin() {
+    registerPlugin(UptrainExposedApiDetector.class);
+  }
+
+  @Provides
+  @UptrainExposedApiOobSleepDuration
+  int provideOobSleepDuration(UptrainExposedApiDetectorConfigs configs) {
+    if (configs.oobSleepDuration == 0) {
+      return 10;
+    }
+    return configs.oobSleepDuration;
+  }
+}
