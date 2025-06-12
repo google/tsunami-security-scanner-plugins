bb4f318f6541d799d5ca4cec99092a57ec44a894
alessandro-Doyensec <alessandro.versari@doyensec.com>
2025-06-12 18:44:30 +0200

    fix: test to reflect new severity

--- doyensec/detectors/comfyui_exposed_ui_detector/src/test/java/com/google/tsunami/plugins/detectors/comfyui/exposed/ComfyUiExposedUiTest.java
+++ doyensec/detectors/comfyui_exposed_ui_detector/src/test/java/com/google/tsunami/plugins/detectors/comfyui/exposed/ComfyUiExposedUiTest.java
@@ -148,7 +148,7 @@ public final class ComfyUiExposedUiTest {
                     VulnerabilityId.newBuilder()
                         .setPublisher(VULNERABILITY_REPORT_PUBLISHER)
                         .setValue("COMFYUI_EXPOSED_UI"))
-                .setSeverity(Severity.CRITICAL)
+                .setSeverity(Severity.HIGH)
                 .setTitle(VULNERABILITY_REPORT_TITLE)
                 .setDescription(VULNERABILITY_REPORT_DESCRIPTION)
                 .setRecommendation(VULNERABILITY_REPORT_RECOMMENDATION))
