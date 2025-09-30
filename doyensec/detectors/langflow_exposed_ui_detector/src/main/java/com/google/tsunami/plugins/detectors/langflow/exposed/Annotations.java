a0115cb74c1daa60664774bc16912959d428e645
Tsunami Team <tsunami-dev@google.com>
2025-09-30 00:38:48 -0700

    No public description
    PiperOrigin-RevId: 813130812
    Change-Id: I3adb87477a02aaa9f42f854721333be08e7971a9

--- doyensec/detectors/langflow_exposed_ui_detector/src/main/java/com/google/tsunami/plugins/detectors/langflow/exposed/Annotations.java
+++ doyensec/detectors/langflow_exposed_ui_detector/src/main/java/com/google/tsunami/plugins/detectors/langflow/exposed/Annotations.java
@@ -16,7 +16,9 @@
 
 package com.google.tsunami.plugins.detectors.langflow.exposed;
 
-import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
